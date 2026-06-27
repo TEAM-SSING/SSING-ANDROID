4bb65748a1bf6bdf53ac2db4634d0e2e9c9bd8af
oilbeaneda <20221444@sungshin.ac.kr>
2026-06-27 18:28:09 +0900

    [init/#1] build-logic/build.gradle 추가

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    `kotlin-dsl`
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.compose.compiler.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+    compileOnly(libs.hilt.gradlePlugin)
+}
+
+gradlePlugin {
+    plugins {
+        register("androidLibrary") {
+            id = "ssing.android.library"
+            implementationClass = "AndroidLibraryConventionPlugin"
+        }
+        register("androidApplication") {
+            id = "ssing.android.application"
+            implementationClass = "AndroidApplicationConventionPlugin"
+        }
+        register("coreUi") {
+            id = "ssing.core.ui"
+            implementationClass = "CoreUiConventionPlugin"
+        }
+        register("coreNetwork") {
+            id = "ssing.core.network"
+            implementationClass = "CoreNetworkConventionPlugin"
+        }
+        register("coreLocalstorage") {
+            id = "ssing.core.localstorage"
+            implementationClass = "CoreLocalstorageConventionPlugin"
+        }
+        register("data") {
+            id = "ssing.data"
+            implementationClass = "DataConventionPlugin"
+        }
+        register("presentation") {
+            id = "ssing.presentation"
+            implementationClass = "PresentationConventionPlugin"
+        }
+    }
+}
