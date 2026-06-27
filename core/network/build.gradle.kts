abf2acdd11429f398c72afedeab68dfae70917b6
oilbeaneda <20221444@sungshin.ac.kr>
2026-06-27 23:49:35 +0900

    [mod/#1]  로컬 프로퍼티 지켜!

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,9 +1,23 @@
+import java.util.Properties
+
 plugins {
     id("ssing.core.network")
 }
 
+val localProperties = Properties().apply {
+    rootProject.file("local.properties").takeIf { it.exists() }?.inputStream()?.use { load(it) }
+}
+
 android {
     namespace = "com.ssing.core.network"
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    defaultConfig {
+        buildConfigField("String", "BASE_URL", "\"${localProperties.getProperty("BASE_URL", "")}\"")
+    }
 }
 
 dependencies {
