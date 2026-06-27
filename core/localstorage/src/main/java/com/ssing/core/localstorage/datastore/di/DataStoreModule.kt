198df7101b33235ca40cada151da57eeddbf11f9
oilbeaneda <20221444@sungshin.ac.kr>
2026-06-27 23:54:03 +0900

    [mod/#1] 파일명 오타 수정 및 싱글톤추가

--- core/localstorage/src/main/java/com/ssing/core/localstorage/datastore/di/DataStoreModule.kt
+++ core/localstorage/src/main/java/com/ssing/core/localstorage/datastore/di/DataStoreModule.kt
@@ -9,6 +9,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -19,6 +20,7 @@ object DataStoreModule {
 
     @TokenDataStore
     @Provides
+    @Singleton
     fun provideTokenDataStore(
         @ApplicationContext context: Context,
     ): DataStore<Preferences> = context.tokenDataStore
