82b38fd6fc0a0f80937b84ece72ae7610b04aa67
oilbeaneda <20221444@sungshin.ac.kr>
2026-06-28 01:18:37 +0900

    [init/#1] 확장함수 추가

---
+++ core/network/src/main/java/com/ssing/core/network/extension/RetrofitExt.kt
@@ -0,0 +1,5 @@
+package com.ssing.core.network.extension
+
+fun String?.isJsonObject(): Boolean = this?.startsWith("{") == true && this.endsWith("}")
+
+fun String?.isJsonArray(): Boolean = this?.startsWith("[") == true && this.endsWith("]")
