b0df41a13cfdce0ba90be57c54ca8ca66c6ef8e8
yeseul <joyri0202@gmail.com>
2026-06-30 19:14:49 +0900

    [feat/#5] SSINGTypography data class 정의

--- core/ui/src/main/java/com/ssing/core/ui/designsystem/theme/Typography.kt
+++ core/ui/src/main/java/com/ssing/core/ui/designsystem/theme/Typography.kt
@@ -1,6 +1,7 @@
 package com.ssing.core.ui.designsystem.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Immutable
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -13,3 +14,21 @@ object PretendardFont {
     val Semibold = FontFamily(Font(R.font.pretendard_semibold))
 }
 
+@Immutable
+data class SSINGTypography(
+    val titleSb28: TextStyle,
+    val titleSb24: TextStyle,
+    val titleMd24: TextStyle,
+    val titleSb22: TextStyle,
+    val titleMd22: TextStyle,
+
+    val bodySb20: TextStyle,
+    val bodyMd20: TextStyle,
+    val bodySb16: TextStyle,
+    val bodyMd16: TextStyle,
+
+    val captionSb14: TextStyle,
+    val captionMd14: TextStyle,
+    val captionSb12: TextStyle,
+    val captionMd12: TextStyle,
+)
