ece64a4260fbfb61f532cf3f1f87c070d2d5813a
yeseul <joyri0202@gmail.com>
2026-06-30 19:11:18 +0900

    [feat/#5] pretendard 폰트 추가

--- core/ui/src/main/java/com/ssing/core/ui/designsystem/theme/Typography.kt
+++ core/ui/src/main/java/com/ssing/core/ui/designsystem/theme/Typography.kt
@@ -2,19 +2,14 @@ package com.ssing.core.ui.designsystem.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.ssing.core.ui.R
 
-// Set of Material typography styles to start with
-val Typography =
-    Typography(
-        bodyLarge =
-            TextStyle(
-                fontFamily = FontFamily.Default,
-                fontWeight = FontWeight.Normal,
-                fontSize = 16.sp,
-                lineHeight = 24.sp,
-                letterSpacing = 0.5.sp,
-            ),
-    )
+object PretendardFont {
+    val Medium = FontFamily(Font(R.font.pretendard_medium))
+    val Semibold = FontFamily(Font(R.font.pretendard_semibold))
+}
+
