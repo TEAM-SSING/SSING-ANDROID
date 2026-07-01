b9be28c0bc9b8fa4bacd492290d3ff27615b3239
apffxhsls <gyeji975@gmail.com>
2026-07-01 22:06:10 +0900

    [feat/#8] fcm 토큰 갱신 콜백 추가

---
+++ core/fcm/src/main/java/com/ssing/core/fcm/MyFirebaseMessagingService.kt
@@ -0,0 +1,10 @@
+package com.ssing.core.fcm
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
+}
