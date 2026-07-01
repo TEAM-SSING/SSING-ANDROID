0f47b0412d72c82faa4d74868f229378c5b08ad0
dodo <eg4rang@naver.com>
2026-07-01 16:38:29 +0900

    [mod/#11] core:ui, presentation에 core:network 의존성 추가 - core:ui : ApiError에 대응되는 공통 uiMessage 확장 프로퍼티 구현 위함 - presentation : 뷰모델에서 ApiError에 대응하기 위함

--- build-logic/convention/src/main/kotlin/CoreUiConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/CoreUiConventionPlugin.kt
@@ -30,6 +30,7 @@ class CoreUiConventionPlugin : Plugin<Project> {
                 add("ksp", libs.findLibrary("hilt.compiler").get())
                 add("implementation", libs.findLibrary("navigation.compose").get())
                 add("implementation", libs.findLibrary("hilt.navigation.compose").get())
+                add("implementation", project(":core:network"))
                 add("lintChecks", libs.findLibrary("compose.lint.checks").get())
             }
         }
