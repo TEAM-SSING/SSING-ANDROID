de1b70a03b6b36ecf504ae93d4cd5c25f17d3b31
dodo <eg4rang@naver.com>
2026-07-01 16:48:50 +0900

    [fix/#11] AppException.uiMessage 문구 수정

--- core/ui/src/main/java/com/ssing/core/ui/extension/ApiErrorExt.kt
+++ core/ui/src/main/java/com/ssing/core/ui/extension/ApiErrorExt.kt
@@ -7,11 +7,11 @@ val ApiException.uiMessage: String
     get() = when (this) {
         is AppException -> this.message ?: "알 수 없는 오류가 발생했어요."
         is ApiException.BadRequest -> this.message ?: "잘못된 요청이에요."
-        is ApiException.Conflict -> this.message ?: "요청한 정보를 찾을 수 없어요."
+        is ApiException.Conflict -> this.message ?: "요청을 처리할 수 없어요."
         is ApiException.Forbidden -> this.message ?: "접근 권한이 없어요."
         is ApiException.InternalServerError -> "알 수 없는 오류가 발생했어요."
         is ApiException.NetworkConnection -> "네트워크 연결이 불안정해요."
-        is ApiException.NotFound -> this.message ?: "요청을 처리할 수 없어요."
+        is ApiException.NotFound -> this.message ?: "요청한 정보를 찾을 수 없어요."
         is ApiException.Unauthorized -> "로그인이 필요해요."
         is ApiException.Unknown -> "알 수 없는 오류가 발생했어요."
     }
