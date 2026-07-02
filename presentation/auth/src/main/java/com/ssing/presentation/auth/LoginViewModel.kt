4c7d3e92c161eb905d552f73f8488ef489791788
doorimng <doorimng@gmail.com>
2026-07-02 17:04:52 +0900

    [feat/#6] 뷰모델 작성

--- presentation/auth/src/main/java/com/ssing/presentation/auth/LoginViewModel.kt
+++ presentation/auth/src/main/java/com/ssing/presentation/auth/LoginViewModel.kt
@@ -1,20 +1,33 @@
 package com.ssing.presentation.auth
 
+import android.content.Context
 import androidx.lifecycle.viewModelScope
 import com.ssing.core.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
-internal class LoginViewModel @Inject constructor() :
+internal class LoginViewModel @Inject constructor(
+    private val kakaoLoginManager: KakaoLoginManager,
+) :
     BaseViewModel<LoginContract.State, LoginContract.Effect>(
         LoginContract.State()
     ) {
 
-    fun onLoginClick() {
+    fun onLoginClick(context: Context) {
         viewModelScope.launch {
-            sendEffect(LoginContract.Effect.NavigateToHome)
+            kakaoLoginManager.login(context) { result ->
+                result.onSuccess { token ->
+                    viewModelScope.launch {
+                        sendEffect(LoginContract.Effect.NavigateToHome)
+                    }
+                    // TODO: 서버 연결 시 token.accessToken 전달
+                }.onFailure { error ->
+                    // TODO: 실패 UI 처리
+                }
+            }
         }
     }
 }
