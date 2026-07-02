9ef3baf520127af1363f070641393df340310146
oilbeaneda <20221444@sungshin.ac.kr>
2026-07-02 22:52:37 +0900

    [mod/#9] TokenAccessManager로 불러오기

--- core/network/src/main/java/com/ssing/core/network/interceptor/AuthInterceptor.kt
+++ core/network/src/main/java/com/ssing/core/network/interceptor/AuthInterceptor.kt
@@ -1,8 +1,8 @@
 package com.ssing.core.network.interceptor
 
-import com.ssing.core.localstorage.datastore.LocalTokenDataSource
 import com.ssing.core.network.constant.AUTHORIZATION
 import com.ssing.core.network.constant.BEARER_PREFIX
+import com.ssing.core.network.token.TokenAccessManager
 import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Response
@@ -12,10 +12,10 @@ import javax.inject.Inject
  * 저장된 Access Token을 모든 요청의 Authorization 헤더에 자동으로 첨부한다.
  */
 internal class AuthInterceptor @Inject constructor(
-    private val tokenDataSource: LocalTokenDataSource,
+    private val tokenAccessManager: TokenAccessManager,
 ) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        val accessToken = runBlocking { tokenDataSource.getAccessToken() }
+        val accessToken = runBlocking { tokenAccessManager.getAccessToken() }
             ?: return chain.proceed(chain.request())
 
         val request = chain.request().newBuilder()
