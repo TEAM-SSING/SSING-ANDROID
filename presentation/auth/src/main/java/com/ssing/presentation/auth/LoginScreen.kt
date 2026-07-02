70a20e3d89aee9aee5fb3da458c0c30c55bfcab1
lmnoo <doorimng@gmail.com>
2026-07-02 14:14:56 +0900

    [feat/#6] 카카오 로그인용 테스트 버튼 생성

--- presentation/auth/src/main/java/com/ssing/presentation/auth/LoginScreen.kt
+++ presentation/auth/src/main/java/com/ssing/presentation/auth/LoginScreen.kt
@@ -1,13 +1,19 @@
 package com.ssing.presentation.auth
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ssing.core.ui.util.HandleUiEffects
@@ -29,6 +35,7 @@ internal fun LoginRoute(
 
     LoginScreen(
         state = state,
+        onKakaoClick = {},
         modifier = modifier,
     )
 }
@@ -36,6 +43,7 @@ internal fun LoginRoute(
 @Composable
 private fun LoginScreen(
     state: LoginContract.State,
+    onKakaoClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
     Box(
@@ -43,6 +51,14 @@ private fun LoginScreen(
         contentAlignment = Alignment.Center,
     ) {
         Text(text = "로그인")
+
+        // 카카오톡 로그인 테스트용 버튼
+        Button(
+            onClick = onKakaoClick,
+            modifier = Modifier.padding(top = 80.dp)
+        ) {
+            Text("임시 로그인 버튼")
+        }
     }
 }
 
@@ -51,5 +67,6 @@ private fun LoginScreen(
 private fun LoginScreenPreview() {
     LoginScreen(
         state = LoginContract.State(),
+        onKakaoClick = {},
     )
 }
