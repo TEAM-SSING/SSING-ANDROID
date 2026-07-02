b246582c0d8c394e7c445a88131a9cb872c462ec
doorimng <doorimng@gmail.com>
2026-07-02 19:47:04 +0900

    [feat/#6] Instructor 카카오 로그인 세팅

--- app/instructor/build.gradle.kts
+++ app/instructor/build.gradle.kts
@@ -1,6 +1,13 @@
+import java.util.Properties
+import kotlin.apply
+
 plugins {
     id("ssing.android.application")
     id("org.jetbrains.kotlin.plugin.serialization")
+}
+
+val properties = Properties().apply {
+    load(rootProject.file("local.properties").inputStream())
 }
 
 android {
@@ -10,6 +17,10 @@ android {
         applicationId = "com.ssing.instructor"
         versionCode = 1
         versionName = "1.0"
+
+        val kakaoNativeAppKey = properties["KAKAO_NATIVE_APP_KEY"].toString()
+        buildConfigField("String", "KAKAO_NATIVE_APP_KEY", "\"$kakaoNativeAppKey\"")
+        manifestPlaceholders["KAKAO_NATIVE_APP_KEY"] = kakaoNativeAppKey
     }
 
     buildFeatures {
@@ -24,4 +35,5 @@ dependencies {
     implementation(projects.presentation.instructorHome)
     implementation(projects.presentation.instructorMatching)
     implementation(libs.immutable)
+    implementation(libs.kakao.user)
 }
