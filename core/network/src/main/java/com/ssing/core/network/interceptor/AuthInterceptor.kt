1dfb65d653f49f1a107bc165071afad9a7fe541b
oilbeaneda <20221444@sungshin.ac.kr>
2026-07-02 12:57:38 +0900

    [feat/#9] AuthInterceptor 구현

---
+++ core/network/src/main/java/com/ssing/core/network/interceptor/AuthInterceptor.kt
@@ -0,0 +1,30 @@
+package com.ssing.core.network.interceptor
+
+import com.ssing.core.localstorage.datastore.LocalTokenDataSource
+import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+
+/**
+ * 저장된 Access Token을 모든 요청의 Authorization 헤더에 자동으로 첨부한다.
+ */
+class AuthInterceptor @Inject constructor(
+    private val tokenDataSource: LocalTokenDataSource,
+) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val accessToken = runBlocking { tokenDataSource.getAccessToken() }
+            ?: return chain.proceed(chain.request())
+
+        val request = chain.request().newBuilder()
+            .header(AUTHORIZATION, "$BEARER_PREFIX$accessToken")
+            .build()
+
+        return chain.proceed(request)
+    }
+
+    companion object {
+        const val AUTHORIZATION = "Authorization"
+        const val BEARER_PREFIX = "Bearer "
+    }
+}
