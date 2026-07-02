a7a50a3298062a4ce959fd9359381b20e7a62959
apffxhsls <gyeji975@gmail.com>
2026-07-03 02:55:41 +0900

    [add/#8] NotificationRequestDto에 @OptIn 추가

--- data/src/main/java/com/ssing/data/remote/dto/NotificationRequestDto.kt
+++ data/src/main/java/com/ssing/data/remote/dto/NotificationRequestDto.kt
@@ -1,9 +1,11 @@
 package com.ssing.data.remote.dto
 
+import kotlinx.serialization.InternalSerializationApi
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
+@OptIn(InternalSerializationApi::class)
 data class NotificationRequestDto(
     @SerialName("token")
     val token: String,
