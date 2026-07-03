b64ee120d46697c5b4e48365d082633cd67a8030
apffxhsls <gyeji975@gmail.com>
2026-07-04 00:23:27 +0900

    [add/#8] notification 모듈 생성 후 파일 이동

--- build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -50,10 +50,6 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
                 add("implementation", libs.findLibrary("androidx.activity.compose").get())
                 add("implementation", libs.findLibrary("navigation.compose").get())
                 add("lintChecks", libs.findLibrary("compose.lint.checks").get())
-                add("implementation", project(":core:fcm"))
-                add("implementation", platform(libs.findLibrary("firebase-bom").get()))
-                add("implementation", libs.findLibrary("firebase-analytics").get())
-                add("implementation", libs.findLibrary("firebase-messaging").get())
             }
         }
     }
---
+++ core/notification/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+    id("ssing.android.library")
+}
+
+android {
+    namespace = "com.ssing.core.notification"
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+    implementation(libs.timber)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.material)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.messaging)
+    implementation(libs.firebase.analytics)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.junit)
+}
