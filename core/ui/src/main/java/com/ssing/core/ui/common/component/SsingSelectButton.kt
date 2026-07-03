6c362a0b60230df2ccc9fcc23d2fa9a46889faca
oilbeaneda <20221444@sungshin.ac.kr>
2026-07-03 11:18:02 +0900

    [mod/#15] 프리뷰 수정

--- core/ui/src/main/java/com/ssing/core/ui/common/component/SsingSelectButton.kt
+++ core/ui/src/main/java/com/ssing/core/ui/common/component/SsingSelectButton.kt
@@ -3,16 +3,14 @@ package com.ssing.core.ui.common.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -74,21 +72,12 @@ fun SsingSelectButton(
 @Preview(showBackground = true)
 @Composable
 private fun SsingSelectButtonPreview() {
-    var selectedIndex by remember { mutableIntStateOf(0) }
+    var isSelected by remember { mutableStateOf(false) }
 
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(20.dp),
-        horizontalArrangement = Arrangement.spacedBy(4.dp),
-    ) {
-        listOf("옵션 1", "옵션 2").forEachIndexed { index, text ->
-            SsingSelectButton(
-                text = text,
-                isSelected = selectedIndex == index,
-                onClick = { selectedIndex = index },
-                modifier = Modifier.weight(1f),
-            )
-        }
-    }
+    SsingSelectButton(
+        text = "옵션",
+        isSelected = isSelected,
+        onClick = { isSelected = !isSelected },
+        modifier = Modifier.width(162.dp),
+    )
 }
