0361b86cb0761ab25f5cb0208b8176caec93890f
apffxhsls <gyeji975@gmail.com>
2026-07-04 23:59:50 +0900

    [chore/#8] NotificationModule에서 변경된 패키지명으로 다시 import

--- core/notification/src/main/java/com/ssing/data/di/NotificationModule.kt
+++ core/notification/src/main/java/com/ssing/data/di/NotificationModule.kt
@@ -1,10 +1,10 @@
 package com.ssing.data.di
 
-import com.ssing.core.notification.data.remote.datasource.NotificationDataSource
-import com.ssing.core.notification.data.repository.NotificationRepository
-import com.ssing.core.notification.data.repository.api.NotificationApi
-import com.ssing.core.notification.data.repository.impl.NotificationDataSourceImpl
-import com.ssing.core.notification.data.repository.impl.NotificationRepositoryImpl
+import com.ssing.data.remote.datasource.NotificationDataSource
+import com.ssing.data.repository.NotificationRepository
+import com.ssing.data.repository.api.NotificationApi
+import com.ssing.data.repository.impl.NotificationDataSourceImpl
+import com.ssing.data.repository.impl.NotificationRepositoryImpl
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
@@ -12,7 +12,6 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import javax.inject.Singleton
-import kotlin.jvm.java
 
 @Module
 @InstallIn(SingletonComponent::class)
