be13b9e4310ba36d45343f02235e0b67b051e889
doorimng <doorimng@gmail.com>
2026-07-06 04:05:55 +0900

    [feat/#39] exception 작성

--- data/src/main/java/com/ssing/data/consumerlogin/exception/ConsumerLoginException.kt
+++ data/src/main/java/com/ssing/data/consumerlogin/exception/ConsumerLoginException.kt
@@ -1,2 +1,32 @@
 package com.ssing.data.consumerlogin.exception
 
+import com.ssing.core.network.exception.BusinessException
+
+sealed class ConsumerLoginException(
+    serverCode: String?,
+    message: String?,
+    requestId: String?,
+) : BusinessException(
+    serverCode = serverCode,
+    message = message,
+    requestId = requestId,
+) {
+
+    class ValidationFailed(
+        serverCode: String?,
+        message: String?,
+        requestId: String?,
+    ) : ConsumerLoginException(serverCode, message, requestId)
+
+    class AuthInvalidKakaoToken(
+        serverCode: String?,
+        message: String?,
+        requestId: String?,
+    ) : ConsumerLoginException(serverCode, message, requestId)
+
+    class ExternalServiceUnavailable(
+        serverCode: String?,
+        message: String?,
+        requestId: String?,
+    ) : ConsumerLoginException(serverCode, message, requestId)
+}
