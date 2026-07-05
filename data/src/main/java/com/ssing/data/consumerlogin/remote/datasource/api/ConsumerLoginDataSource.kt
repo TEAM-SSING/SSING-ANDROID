9a6912e22756e5afca9649ad51c1090fffbcf7cf
doorimng <doorimng@gmail.com>
2026-07-06 02:39:56 +0900

    [feat/#39] datasource 작성

--- data/src/main/java/com/ssing/data/consumerlogin/remote/datasource/api/ConsumerLoginDataSource.kt
+++ data/src/main/java/com/ssing/data/consumerlogin/remote/datasource/api/ConsumerLoginDataSource.kt
@@ -1,2 +1,10 @@
-package com.ssing.data.consumerlogin.remote.api
+package com.ssing.data.consumerlogin.remote.datasource.api
 
+import com.ssing.core.network.model.BaseResponse
+import com.ssing.data.consumerlogin.remote.dto.ConsumerLoginResponse
+
+interface ConsumerLoginDataSource {
+    suspend fun postConsumerKakaoLogin(
+        kakaoAccessToken: String,
+    ): BaseResponse<ConsumerLoginResponse>
+}
--- data/src/main/java/com/ssing/data/consumerlogin/remote/datasource/impl/ConsumerLoginDataSourceImpl.kt
+++ data/src/main/java/com/ssing/data/consumerlogin/remote/datasource/impl/ConsumerLoginDataSourceImpl.kt
@@ -1,2 +1,18 @@
-package com.ssing.data.consumerlogin.remote.impl
+package com.ssing.data.consumerlogin.remote.datasource.impl
 
+import com.ssing.core.network.model.BaseResponse
+import com.ssing.data.consumerlogin.remote.datasource.api.ConsumerLoginDataSource
+import com.ssing.data.consumerlogin.remote.dto.ConsumerLoginRequest
+import com.ssing.data.consumerlogin.remote.dto.ConsumerLoginResponse
+import com.ssing.data.consumerlogin.remote.service.ConsumerLoginService
+import javax.inject.Inject
+
+internal class ConsumerLoginDataSourceImpl @Inject constructor(
+    private val consumerLoginService: ConsumerLoginService,
+) : ConsumerLoginDataSource {
+
+    override suspend fun postConsumerKakaoLogin(kakaoAccessToken: String): BaseResponse<ConsumerLoginResponse> =
+        consumerLoginService.postConsumerKakaoLogin(
+            ConsumerLoginRequest(kakaoAccessToken),
+        )
+}
