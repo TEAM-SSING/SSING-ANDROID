b208031f1c32ac1de0ae526e907f3454b72c2048
doorimng <doorimng@gmail.com>
2026-07-06 02:50:20 +0900

    [feat/#39] model 작성

--- data/src/main/java/com/ssing/data/consumerlogin/model/ConsumerLoginResult.kt
+++ data/src/main/java/com/ssing/data/consumerlogin/model/ConsumerLoginResult.kt
@@ -1,2 +1,12 @@
 package com.ssing.data.consumerlogin.model
 
+data class ConsumerLoginResult(
+    val accessToken: String,
+    val refreshToken: String,
+    val tokenType: String,
+    val expiresIn: Long,
+    val memberId: Long,
+    val nickname: String,
+    val role: String,
+    val memberStatus: String,
+)
