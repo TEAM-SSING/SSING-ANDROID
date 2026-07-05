69c49d6fcfeb287e2d6782030924860190d1bb4b
doorimng <doorimng@gmail.com>
2026-07-06 03:50:20 +0900

    [feat/#39] repository 작성

--- data/src/main/java/com/ssing/data/consumerlogin/repository/api/ConsumerLoginRepository.kt
+++ data/src/main/java/com/ssing/data/consumerlogin/repository/api/ConsumerLoginRepository.kt
@@ -1,2 +1,10 @@
 package com.ssing.data.consumerlogin.repository.api
 
+import com.ssing.core.network.model.BaseResponse
+import com.ssing.data.consumerlogin.remote.dto.ConsumerLoginResponse
+
+interface ConsumerLoginRepository {
+    suspend fun postConsumerKakaoLogin(
+        kakaoAccessToken: String,
+    ): BaseResponse<ConsumerLoginResponse>
+}
--- data/src/main/java/com/ssing/data/consumerlogin/repository/impl/ConsumerLoginRepositoryImpl.kt
+++ data/src/main/java/com/ssing/data/consumerlogin/repository/impl/ConsumerLoginRepositoryImpl.kt
@@ -1,2 +1,16 @@
 package com.ssing.data.consumerlogin.repository.impl
 
+import com.ssing.core.network.model.BaseResponse
+import com.ssing.data.consumerlogin.remote.datasource.api.ConsumerLoginDataSource
+import com.ssing.data.consumerlogin.remote.dto.ConsumerLoginResponse
+import com.ssing.data.consumerlogin.repository.api.ConsumerLoginRepository
+import javax.inject.Inject
+
+internal class ConsumerLoginRepositoryImpl @Inject constructor(
+    private val dataSource: ConsumerLoginDataSource,
+) : ConsumerLoginRepository {
+
+    override suspend fun postConsumerKakaoLogin(
+        kakaoAccessToken: String,
+    ): BaseResponse<ConsumerLoginResponse> = dataSource.postConsumerKakaoLogin(kakaoAccessToken)
+}
