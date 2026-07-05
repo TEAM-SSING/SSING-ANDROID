590bbc0c7e3e98dcdcbaff4d9bc83a5d0edbfc4a
doorimng <doorimng@gmail.com>
2026-07-06 02:23:31 +0900

    [feat/#39] dto 작성

--- data/src/main/java/com/ssing/data/consumerlogin/remote/dto/ConsumerLoginRequest.kt
+++ data/src/main/java/com/ssing/data/consumerlogin/remote/dto/ConsumerLoginRequest.kt
@@ -1,2 +1,10 @@
 package com.ssing.data.consumerlogin.remote.dto
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ConsumerLoginRequest(
+    @SerialName("kakaoAccessToken")
+    val kakaoAccessToken: String,
+)
--- data/src/main/java/com/ssing/data/consumerlogin/remote/dto/ConsumerLoginResponse.kt
+++ data/src/main/java/com/ssing/data/consumerlogin/remote/dto/ConsumerLoginResponse.kt
@@ -1,2 +1,30 @@
 package com.ssing.data.consumerlogin.remote.dto
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ConsumerLoginResponse(
+    @SerialName("accessToken")
+    val accessToken: String,
+    @SerialName("refreshToken")
+    val refreshToken: String,
+    @SerialName("tokenType")
+    val tokenType: String,
+    @SerialName("expiresIn")
+    val expiresIn: Long,
+    @SerialName("member")
+    val member: ConsumerMemberResponse,
+)
+
+@Serializable
+data class ConsumerMemberResponse(
+    @SerialName("id")
+    val id: Long,
+    @SerialName("nickname")
+    val nickname: String,
+    @SerialName("role")
+    val role: String,
+    @SerialName("memberStatus")
+    val memberStatus: String,
+)
