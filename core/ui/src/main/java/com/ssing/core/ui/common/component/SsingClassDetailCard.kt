121c4c96493b65d9c79314e2c1b7011ff25b839a
apffxhsls <gyeji975@gmail.com>
2026-07-07 01:34:45 +0900

    [mod/#41] 강습 인원에 따른 FlowRow 조절

--- core/ui/src/main/java/com/ssing/core/ui/common/component/SsingClassDetailCard.kt
+++ core/ui/src/main/java/com/ssing/core/ui/common/component/SsingClassDetailCard.kt
@@ -217,10 +217,19 @@ private fun SsingPriceRow(label: String, isPaid: Boolean, price: Int) {
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
 private fun SsingParticipantsRow(label: String, participants: List<String>) {
+    val verticalAlignment = if (participants.size <= 3) {
+        Alignment.CenterVertically
+    } else {
+        Alignment.Top
+    }
+    val maxItemsInEachRow = when (participants.size) {
+        4 -> 2
+        else -> 3
+    }
     Row(
         modifier = Modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically,
+        verticalAlignment = verticalAlignment,
     ) {
         Text(
             text = label,
@@ -231,7 +240,7 @@ private fun SsingParticipantsRow(label: String, participants: List<String>) {
             modifier = Modifier.weight(1f),
             horizontalArrangement = Arrangement.End,
             verticalArrangement = Arrangement.spacedBy(4.dp),
-            maxItemsInEachRow = 3,
+            maxItemsInEachRow = maxItemsInEachRow,
         ) {
             participants.forEach { participant ->
                 SsingChip(text = participant, style = SsingChipStyle.GRAY)
@@ -308,7 +317,7 @@ private fun SsingClassDetailCardPreview() {
                 location = "OOO 리조트",
                 duration = "0시간",
                 maxCapacity = 0,
-                participants = listOf("11세 남", "11세 남", "9세 여", "9세 여", "9세 여"),
+                participants = listOf("11세 남", "11세 남", "9세 여"),
                 isPaid = true,
                 price = 0, // 수정
                 equipmentStatus = "착용 완료",
