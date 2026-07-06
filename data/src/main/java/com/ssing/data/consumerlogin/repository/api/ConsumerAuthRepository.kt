19265d2256d26f52d27dabfe5bbb02e7598845f4
doorimng <doorimng@gmail.com>
2026-07-07 05:08:40 +0900

    [refactor/#39] Repository에 DTO를 노출하지 않도록 반환값 수정

--- data/src/main/java/com/ssing/data/consumerlogin/repository/api/ConsumerAuthRepository.kt
+++ data/src/main/java/com/ssing/data/consumerlogin/repository/api/ConsumerAuthRepository.kt
@@ -1,10 +1,7 @@
 package com.ssing.data.consumerlogin.repository.api
 
-import com.ssing.core.network.model.BaseResponse
-import com.ssing.data.consumerlogin.remote.dto.ConsumerAuthResponse
-
 interface ConsumerAuthRepository {
     suspend fun postConsumerKakaoAuth(
         kakaoAccessToken: String,
-    ): BaseResponse<ConsumerAuthResponse>
+    ): Result<Unit>
 }
--- data/src/main/java/com/ssing/data/consumerlogin/repository/impl/ConsumerAuthRepositoryImpl.kt
+++ data/src/main/java/com/ssing/data/consumerlogin/repository/impl/ConsumerAuthRepositoryImpl.kt
@@ -1,16 +1,24 @@
 package com.ssing.data.consumerlogin.repository.impl
 
-import com.ssing.core.network.model.BaseResponse
+import com.ssing.core.network.token.TokenAccessManager
+import com.ssing.core.network.util.ApiResponseHandler
 import com.ssing.data.consumerlogin.remote.datasource.api.ConsumerAuthDataSource
-import com.ssing.data.consumerlogin.remote.dto.ConsumerAuthResponse
 import com.ssing.data.consumerlogin.repository.api.ConsumerAuthRepository
 import javax.inject.Inject
 
 internal class ConsumerAuthRepositoryImpl @Inject constructor(
+    private val apiResponseHandler: ApiResponseHandler,
     private val dataSource: ConsumerAuthDataSource,
+    private val tokenAccessManager: TokenAccessManager,
 ) : ConsumerAuthRepository {
 
-    override suspend fun postConsumerKakaoAuth(
-        kakaoAccessToken: String,
-    ): BaseResponse<ConsumerAuthResponse> = dataSource.postConsumerKakaoAuth(kakaoAccessToken)
+    override suspend fun postConsumerKakaoAuth(kakaoAccessToken: String): Result<Unit> =
+        apiResponseHandler.safeApiCall {
+            dataSource.postConsumerKakaoAuth(kakaoAccessToken)
+        }.onSuccess { response ->
+            tokenAccessManager.withLock {
+                setAccessToken(response.accessToken)
+                setRefreshToken(response.refreshToken)
+            }
+        }.map { Unit }
 }
