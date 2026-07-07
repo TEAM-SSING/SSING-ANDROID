e205635b3a5df06bd502a33b61794aa1b8bf4590
oilbeaneda <20221444@sungshin.ac.kr>
2026-07-07 16:12:08 +0900

    [feat/#53] MatchingContract 구현

---
+++ presentation/instructor-matching/src/main/java/com/ssing/presentation/instructormatching/MatchingContract.kt
@@ -0,0 +1,41 @@
+package com.ssing.presentation.instructormatching
+
+import androidx.compose.runtime.Immutable
+import com.ssing.presentation.instructormatching.model.ConditionUiState
+import com.ssing.presentation.instructormatching.model.MatchingOfferUiModel
+
+internal interface MatchingContract {
+
+    @Immutable
+    data class State(
+        val phase: MatchingPhase = MatchingPhase.SettingCondition,
+        val condition: ConditionUiState = ConditionUiState(),
+        val dialog: MatchingDialog? = null,
+    )
+
+    sealed interface MatchingPhase {
+        data object SettingCondition : MatchingPhase
+
+        data object Waiting : MatchingPhase
+
+        data class OfferArrived(val offer: MatchingOfferUiModel) : MatchingPhase
+
+        data class PendingConfirm(
+            val offer: MatchingOfferUiModel,
+            val confirmationExpiresAtMillis: Long? = null,
+        ) : MatchingPhase
+    }
+
+    sealed interface MatchingDialog {
+        data object StopWaiting : MatchingDialog
+
+        data object ConsumerRejected : MatchingDialog
+
+        data object MatchingFailed : MatchingDialog
+    }
+
+    sealed interface Effect {
+        data class ShowToast(val message: String) : Effect
+        data object NavigateBack : Effect
+    }
+}
