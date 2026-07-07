53a78ad03d13691b4d5ae3c7d8f61d0988677366
dodo <eg4rang@naver.com>
2026-07-07 23:26:36 +0900

    [feat/#72] 소비자 NavHost에 소비자 매칭 그래프 연결

--- app/consumer/src/main/java/com/ssing/consumer/ConsumerMainNavHost.kt
+++ app/consumer/src/main/java/com/ssing/consumer/ConsumerMainNavHost.kt
@@ -6,6 +6,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import com.presentation.consumermatching.navigation.ConsumerMatchingCondition
+import com.presentation.consumermatching.navigation.consumerMatchingNavGraph
 import com.ssing.core.ui.extension.clearBackStackNavOptions
 import com.ssing.presentation.auth.navigation.Login
 import com.ssing.presentation.auth.navigation.authNavGraph
@@ -33,6 +35,10 @@ internal fun ConsumerMainNavHost(
             },
         )
         consumerHomeNavGraph(
+            paddingValues = paddingValues,
+        )
+        consumerMatchingNavGraph(
+            navController = navController,
             paddingValues = paddingValues,
         )
     }
