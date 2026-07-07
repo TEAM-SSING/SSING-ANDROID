00e053cae74095bfba3f32df258e76c47f066c8c
yeseul <joyri0202@gmail.com>
2026-07-07 14:42:53 +0900

    [mod/#38] loginWithKakao Result 반환타입 수정

--- data/src/main/java/com/ssing/data/auth/repository/api/InstructorLoginRepository.kt
+++ data/src/main/java/com/ssing/data/auth/repository/api/InstructorLoginRepository.kt
@@ -1,7 +1,5 @@
 package com.ssing.data.auth.repository.api
 
-import com.ssing.data.auth.model.InstructorLoginResult
-
 interface InstructorLoginRepository {
-    suspend fun loginWithKakao(kakaoAccessToken: String): Result<InstructorLoginResult>
+    suspend fun loginWithKakao(kakaoAccessToken: String): Result<Unit>
 }
