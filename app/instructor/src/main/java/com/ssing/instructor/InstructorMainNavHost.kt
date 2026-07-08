45cbcf297216b1d138353841e96d08784c7649bd
oilbeaneda <20221444@sungshin.ac.kr>
2026-07-08 15:31:30 +0900

    [feat/#74] 네비 그래프 연결

--- app/instructor/src/main/java/com/ssing/instructor/InstructorMainNavHost.kt
+++ app/instructor/src/main/java/com/ssing/instructor/InstructorMainNavHost.kt
@@ -10,6 +10,7 @@ import com.ssing.core.ui.extension.clearBackStackNavOptions
 import com.ssing.presentation.auth.navigation.authNavGraph
 import com.ssing.presentation.instructorhome.navigation.InstructorHome
 import com.ssing.presentation.instructorhome.navigation.instructorHomeNavGraph
+import com.ssing.presentation.notification.navigation.notificationNavGraph
 
 @Composable
 internal fun InstructorMainNavHost(
@@ -32,6 +33,12 @@ internal fun InstructorMainNavHost(
             },
         )
         instructorHomeNavGraph(paddingValues = paddingValues)
-        // TODO 나중에 각 presentation 모듈 navGraph 연결
+        notificationNavGraph(
+            paddingValues = paddingValues,
+            navController = navController,
+            navigateToLessonArrival = {},
+            navigateToMatchingLoading = {},
+            navigateToLessonReady = {},
+        )
     }
 }
