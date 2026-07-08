441b5698210870508db43c13447f3b883da4a242
apffxhsls <gyeji975@gmail.com>
2026-07-08 23:40:36 +0900

    [feat/#66] InstructorLessonNavigation 구현

---
+++ presentation/instructor-lesson/src/main/java/instructorlessondetail/navigation/InstructorLessonNavigation.kt
@@ -0,0 +1,26 @@
+package instructorlessondetail.navigation
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.ssing.core.ui.navigation.MainTabRoute
+import instructorlessondetail.screen.LessonDetailRoute
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object InstructorLesson: MainTabRoute
+
+fun NavGraphBuilder.instructorLessonNavGraph(
+    paddingValues: PaddingValues,
+    navController: NavController,
+) {
+    composable<InstructorLesson> {
+        LessonDetailRoute(
+            navigateBack = { navController.popBackStack() },
+            modifier = Modifier.padding(paddingValues),
+        )
+    }
+}
