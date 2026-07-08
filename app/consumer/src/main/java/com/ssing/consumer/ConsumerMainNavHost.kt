05f1c7a2228b4eb89cf3ab88e40093f63aa92459
oilbeaneda <20221444@sungshin.ac.kr>
2026-07-08 14:26:44 +0900

    [feat/#74] 네비 연결

--- app/consumer/src/main/java/com/ssing/consumer/ConsumerMainNavHost.kt
+++ app/consumer/src/main/java/com/ssing/consumer/ConsumerMainNavHost.kt
@@ -11,6 +11,7 @@ import com.ssing.presentation.auth.navigation.Login
 import com.ssing.presentation.auth.navigation.authNavGraph
 import com.ssing.presentation.consumerhome.navigation.ConsumerHome
 import com.ssing.presentation.consumerhome.navigation.consumerHomeNavGraph
+import com.ssing.presentation.notification.navigation.notificationNavGraph
 
 @Composable
 internal fun ConsumerMainNavHost(
@@ -34,6 +35,14 @@ internal fun ConsumerMainNavHost(
         )
         consumerHomeNavGraph(
             paddingValues = paddingValues,
+        )
+        notificationNavGraph(
+            paddingValues = paddingValues,
+            navController = navController,
+            // TODO: 대상 화면(강습 도착 / 매칭 로딩중 / 강습전) 구현 후 실제 라우트로 연결
+            navigateToLessonArrival = { },
+            navigateToMatchingLoading = { },
+            navigateToLessonReady = { },
         )
     }
 }
---
+++ presentation/notification/src/main/java/com/ssing/presentation/notification/navigation/NotificationNavigation.kt
@@ -0,0 +1,38 @@
+package com.ssing.presentation.notification.navigation
+
+import android.app.Notification
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.ssing.core.ui.navigation.Route
+import com.ssing.presentation.notification.NotificationRoute
+import kotlinx.serialization.Serializable
+
+fun NavController.navigateToNotification(navOptions: NavOptions? = null) {
+    navigate(route = Notification, navOptions = navOptions)
+}
+
+fun NavGraphBuilder.notificationNavGraph(
+    paddingValues: PaddingValues,
+    navController: NavController,
+    navigateToLessonArrival: () -> Unit,
+    navigateToMatchingLoading: () -> Unit,
+    navigateToLessonReady: () -> Unit,
+) {
+    composable<Notification> {
+        NotificationRoute(
+            navigateBack = { navController.popBackStack() },
+            navigateToLessonArrival = navigateToLessonArrival,
+            navigateToMatchingLoading = navigateToMatchingLoading,
+            navigateToLessonReady = navigateToLessonReady,
+            modifier = Modifier.padding(paddingValues),
+        )
+    }
+}
+
+@Serializable
+data object Notification : Route
