5b74f7735460de3887f0bcf19a71b765fd816d24
doorimng <doorimng@gmail.com>
2026-07-09 04:20:29 +0900

    [refactor/#81] InfoText에 color 파라미터 추가

--- presentation/auth/src/main/java/com/ssing/presentation/auth/component/InfoText.kt
+++ presentation/auth/src/main/java/com/ssing/presentation/auth/component/InfoText.kt
@@ -1,11 +1,17 @@
 package com.ssing.presentation.auth.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ssing.core.ui.designsystem.theme.SSINGTheme
+import com.ssing.core.ui.extension.noRippleClickable
 
 @Composable
 internal fun InfoText(
@@ -17,7 +23,29 @@ internal fun InfoText(
     Text(
         text = text,
         style = SSINGTheme.typography.caption.md12,
-        modifier = modifier.clickable(onClick = onClick),
+        modifier = modifier.noRippleClickable(onClick = onClick),
         color = color,
     )
 }
+
+@Preview(showBackground = true)
+@Composable
+private fun InfoTextPreview(){
+    SSINGTheme{
+        Column() {
+            InfoText(
+                text = "이용약관",
+                onClick = {},
+            )
+
+            InfoText(
+                text = "이용약관",
+                onClick = {},
+                color = SSINGTheme.colors.primaryAlternative,
+                modifier = Modifier.background(color = SSINGTheme.colors.primaryNormal)
+            )
+        }
+
+    }
+}
+
