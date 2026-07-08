0efd2d75ea5706807140409e2a5e468a4d682812
doorimng <doorimng@gmail.com>
2026-07-08 21:53:40 +0900

    [feat/#81] Background 색상 지정

--- presentation/auth/src/main/java/com/ssing/presentation/auth/consumer/ConsumerLoginScreen.kt
+++ presentation/auth/src/main/java/com/ssing/presentation/auth/consumer/ConsumerLoginScreen.kt
@@ -2,6 +2,7 @@ package com.ssing.presentation.auth.consumer
 
 import androidx.activity.compose.LocalActivity
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -102,6 +103,7 @@ private fun ConsumerLoginScreen(
     Column(
         modifier = modifier
             .fillMaxSize()
+            .background(color = SSINGTheme.colors.backgroundNormal)
             .padding(horizontal = 16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
