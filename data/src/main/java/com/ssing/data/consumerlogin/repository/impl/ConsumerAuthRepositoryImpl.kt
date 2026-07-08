50cd0f2f0ac51c9b2c5dd349f6f07c9618bdcc95
doorimng <doorimng@gmail.com>
2026-07-09 05:03:30 +0900

    [refactor/#39] dataSource 호출 시 크래시 발생 위험 해결

--- data/src/main/java/com/ssing/data/consumerlogin/repository/impl/ConsumerAuthRepositoryImpl.kt
+++ data/src/main/java/com/ssing/data/consumerlogin/repository/impl/ConsumerAuthRepositoryImpl.kt
@@ -1,9 +1,13 @@
 package com.ssing.data.consumerlogin.repository.impl
 
+import com.ssing.core.network.extension.mapApiException
 import com.ssing.core.network.token.TokenAccessManager
 import com.ssing.core.network.util.ApiResponseHandler
+import com.ssing.core.network.util.suspendRunCatching
+import com.ssing.data.consumerlogin.exception.ConsumerAuthException
 import com.ssing.data.consumerlogin.remote.datasource.api.ConsumerAuthDataSource
 import com.ssing.data.consumerlogin.repository.api.ConsumerAuthRepository
+import timber.log.Timber
 import javax.inject.Inject
 
 internal class ConsumerAuthRepositoryImpl @Inject constructor(
@@ -15,10 +19,42 @@ internal class ConsumerAuthRepositoryImpl @Inject constructor(
     override suspend fun postConsumerKakaoAuth(kakaoAccessToken: String): Result<Unit> =
         apiResponseHandler.safeApiCall {
             dataSource.postConsumerKakaoAuth(kakaoAccessToken)
-        }.onSuccess { response ->
-            tokenAccessManager.withLock {
-                setAccessToken(response.accessToken)
-                setRefreshToken(response.refreshToken)
+        }.mapCatching { response ->
+            suspendRunCatching {
+                tokenAccessManager.withLock {
+                    setAccessToken(response.accessToken)
+                    setRefreshToken(response.refreshToken)
+                }
+            }.onFailure { throwable ->
+                suspendRunCatching {
+                    tokenAccessManager.withLock {
+                        clearTokens()
+                    }
+                }.onFailure { Timber.e(it, "clearTokens 실패") }
+                throw throwable
             }
-        }.map { Unit }
+        }.map { }
+            .mapApiException {
+                when (it.serverCode) {
+                    "VALIDATION_FAILED" -> ConsumerAuthException.ValidationFailed(
+                        it.serverCode,
+                        it.message,
+                        it.requestId
+                    )
+
+                    "AUTH_INVALID_KAKAO_TOKEN" -> ConsumerAuthException.AuthInvalidKakaoToken(
+                        it.serverCode,
+                        it.message,
+                        it.requestId
+                    )
+
+                    "EXTERNAL_SERVICE_UNAVAILABLE" -> ConsumerAuthException.ExternalServiceUnavailable(
+                        it.serverCode,
+                        it.message,
+                        it.requestId
+                    )
+
+                    else -> it
+                }
+            }
 }
