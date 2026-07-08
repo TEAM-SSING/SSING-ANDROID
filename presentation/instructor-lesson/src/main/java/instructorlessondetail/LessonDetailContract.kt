f5f2f1d5f29fe8b4f182acb9b77d024bfc69b8dc
apffxhsls <gyeji975@gmail.com>
2026-07-08 20:53:48 +0900

    [feat/#66] LessonDetailContract 구현

--- presentation/instructor-lesson/src/main/java/instructorlessondetail/LessonDetailContract.kt
+++ presentation/instructor-lesson/src/main/java/instructorlessondetail/LessonDetailContract.kt
@@ -9,6 +9,7 @@ internal interface LessonDetailContract {
 
     @Immutable
     data class State(
+        val phase: LessonDetailPhase = LessonDetailPhase.settingCondition,
         val isLoading: Boolean = false,
         val isRequesting: Boolean = false,
         val instructorId: Long = 0L,
@@ -27,13 +28,22 @@ internal interface LessonDetailContract {
         val teams: ImmutableList<TeamParticipantsInfo> = persistentListOf(),
     )
 
+    sealed interface LessonDetailPhase {
+        data object settingCondition : LessonDetailPhase
+        data object matching : LessonDetailPhase
+        data object lessonInProgress : LessonDetailPhase
+    }
+
+    sealed interface LessonDetailDialog {
+        data object StopWaiting : LessonDetailDialog
+        data object ConsumerRejected : LessonDetailDialog
+        data object LessonCanceled : LessonDetailDialog
+    }
+
     sealed interface Effect {
         data object NavigateBack : Effect
         data class ShowToast(val message: String) : Effect
-        data class NavigateToRequestMatching(val instructorId: Long) : Effect
-        data object ShowErrorDialog : Effect
 
         data object ShowCancelClassDialog : Effect
-        data object NavigateToChatRoom : Effect
     }
 }
