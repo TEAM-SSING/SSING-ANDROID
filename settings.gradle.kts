abb8f942a0a784dc3299645bf214e1f646713c5f
oilbeaneda <20221444@sungshin.ac.kr>
2026-07-10 05:00:09 +0900

    [feat/#80] 앱 setting.gradle 에 추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.initialization.resolve.RepositoriesMode
+
 pluginManagement {
     includeBuild("build-logic")
     repositories {
@@ -42,3 +44,5 @@ include(":presentation:instructor-matching")
 include(":app:consumer")
 include(":app:instructor")
 include(":core:notification")
+include(":presentation:consumer-profile")
+include(":presentation:instructor-profile")
