e19add136c177f0f0bac8555bb269c0526ae634d
oilbeaneda <20221444@sungshin.ac.kr>
2026-07-09 18:35:55 +0900

    [feat/#80] 영상찍기위해 성공처리해두었던거 롤백

--- presentation/instructor-profile/src/main/java/com/ssing/presentation/instructorprofile/ProfileViewModel.kt
+++ presentation/instructor-profile/src/main/java/com/ssing/presentation/instructorprofile/ProfileViewModel.kt
@@ -16,7 +16,18 @@ internal class ProfileViewModel @Inject constructor(
 ) {
 
     fun onLogoutClick() {
+        if (uiState.value.isLoading) return
+        
+        updateState { copy(isLoading = true) }
         viewModelScope.launch {
+            logoutRepository.logout()
+                .onSuccess { sendEffect(ProfileContract.Effect.NavigateToLogin) }
+                .onFailure {
+                    Timber.e(it, "로그아웃 실패")
+                    sendEffect(ProfileContract.Effect.ShowToast("로그아웃에 실패했습니다."))
+                }
+
+            updateState { copy(isLoading = false) }
             sendEffect(ProfileContract.Effect.NavigateToLogin)
         }
     }
