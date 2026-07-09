bdb1ce9a61aae5bf920c313b50fdb93b4a77af92
yeseul <joyri0202@gmail.com>
2026-07-09 17:21:06 +0900

    [feat/#77] ConsumerLessonViewModel 추가

---
+++ presentation/consumer-lesson/src/main/java/com/ssing/presentation/consumerlesson/ConsumerLessonViewModel.kt
@@ -0,0 +1,32 @@
+package com.ssing.presentation.consumerlesson
+
+import androidx.lifecycle.viewModelScope
+import com.ssing.core.ui.base.BaseViewModel
+import com.ssing.core.ui.common.component.LessonBannerState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+internal class ConsumerLessonViewModel @Inject constructor() :
+    BaseViewModel<ConsumerLessonContract.State, ConsumerLessonContract.Effect>(
+        ConsumerLessonContract.State()
+    ) {
+    fun onReadyClick() {
+        updateState { copy(isReady = true) }
+    }
+
+    fun onCancelClick() {
+        updateState { copy(showCancelConfirmSheet = true) }
+    }
+
+    fun onCancelConfirmed() {
+        viewModelScope.launch {
+            updateState { copy(lessonBannerState = LessonBannerState.Canceled) }
+        }
+    }
+
+    fun onCancelDismiss() {
+        updateState { copy(showCancelConfirmSheet = false) }
+    }
+}
