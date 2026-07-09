c629fb76cdd6cb687f562a7eacc119a09f4d9004
dodo <eg4rang@naver.com>
2026-07-09 15:20:02 +0900

    [feat/#95] 강습생 정보 입력 뷰모델 로직 구현

--- presentation/consumer-matching/src/main/java/com/ssing/presentation/consumermatching/condition/ConsumerMatchingConditionViewModel.kt
+++ presentation/consumer-matching/src/main/java/com/ssing/presentation/consumermatching/condition/ConsumerMatchingConditionViewModel.kt
@@ -1,11 +1,15 @@
 package com.ssing.presentation.consumermatching.condition
 
 import com.ssing.core.ui.base.BaseViewModel
+import com.ssing.presentation.consumermatching.type.ConsumerGender
+import kotlinx.collections.immutable.toPersistentList
 
 internal class ConsumerMatchingConditionViewModel :
     BaseViewModel<ConsumerMatchingConditionContract.State, ConsumerMatchingConditionContract.Effect>(
         initialState = ConsumerMatchingConditionContract.State(),
     ) {
+    private var nextConsumerId = 1
+
     fun onResortSelect(resort: Resort) =
         updateState { copy(selectedResort = resort) }
 
@@ -27,6 +31,35 @@ internal class ConsumerMatchingConditionViewModel :
 
     fun onConfirm(isChecked: Boolean) =
         updateState { copy(isConfirmed = isChecked) }
+
+    fun onAddConsumerClick() {
+        updateState {
+            copy(consumers = consumers.add(ConsumerInfo(id = nextConsumerId++)))
+        }
+    }
+
+    fun onConsumerDelete(id: Int) {
+        if (uiState.value.consumers.size == 1) return
+        updateState { copy(consumers = consumers.removeAll { it.id == id }) }
+    }
+
+    fun onConsumerGenderSelect(id: Int, gender: ConsumerGender) =
+        updateState {
+            copy(
+                consumers = consumers.map {
+                    if (it.id == id) it.copy(gender = gender) else it
+                }.toPersistentList(),
+            )
+        }
+
+    fun onConsumerFocusChange(id: Int, isFocused: Boolean) =
+        updateState {
+            copy(
+                consumers = consumers.map {
+                    it.copy(isFocused = it.id == id && isFocused)
+                }.toPersistentList(),
+            )
+        }
 
     fun onStartMatchingClick() {
         sendEffect(ConsumerMatchingConditionContract.Effect.NavigateToMatching)
