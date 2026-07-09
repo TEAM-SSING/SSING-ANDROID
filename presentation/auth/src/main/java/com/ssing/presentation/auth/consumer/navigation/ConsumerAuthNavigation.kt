d17edb6d7cd6daa14b5ab8957762740c1475496c
doorimng <doorimng@gmail.com>
2026-07-10 00:58:12 +0900

    [mod/#81] ConsumerLogin에 MainTabRoute 추가

--- presentation/auth/src/main/java/com/ssing/presentation/auth/consumer/navigation/ConsumerAuthNavigation.kt
+++ presentation/auth/src/main/java/com/ssing/presentation/auth/consumer/navigation/ConsumerAuthNavigation.kt
@@ -5,11 +5,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import com.ssing.core.ui.navigation.MainTabRoute
 import com.ssing.presentation.auth.consumer.ConsumerLoginRoute
 import kotlinx.serialization.Serializable
 
 @Serializable
-data object ConsumerLogin
+data object ConsumerLogin : MainTabRoute
 
 fun NavGraphBuilder.consumerAuthNavGraph(
     paddingValues: PaddingValues,
