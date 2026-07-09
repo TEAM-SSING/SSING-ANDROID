56743c652b94c1020bbdc6417082cbe9d953b3b9
apffxhsls <gyeji975@gmail.com>
2026-07-10 07:39:32 +0900

    [del/#66] @OptIn(ExperimentalMaterial3Api::class) 삭제

--- presentation/instructor-lesson/src/main/java/com/ssing/presentation/instructorlessondetail/screen/LessonDetailCanceledScreen.kt
+++ presentation/instructor-lesson/src/main/java/com/ssing/presentation/instructorlessondetail/screen/LessonDetailCanceledScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -34,7 +33,6 @@ import com.ssing.presentation.instructorlessondetail.model.LessonDetailCanceledU
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.collections.immutable.toPersistentList
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun LessonDetailCanceledScreen(
     cancel: LessonDetailCanceledUiModel,
