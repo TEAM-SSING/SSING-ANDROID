fb1d0203ae99889ef0f2c47a680d50d0c37da461
yeseul <joyri0202@gmail.com>
2026-07-10 02:32:29 +0900

    [mod/#88] Route 추가

--- presentation/auth/src/main/java/com/ssing/presentation/auth/instructor/navigation/InstructorLoginNavigation.kt
+++ presentation/auth/src/main/java/com/ssing/presentation/auth/instructor/navigation/InstructorLoginNavigation.kt
@@ -2,11 +2,12 @@ package com.ssing.presentation.auth.instructor.navigation
 
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import com.ssing.core.ui.navigation.Route
 import com.ssing.presentation.auth.instructor.InstructorLoginRoute
 import kotlinx.serialization.Serializable
 
 @Serializable
-data object InstructorLogin
+data object InstructorLogin : Route
 
 fun NavGraphBuilder.instructorAuthNavGraph(
     navigateToHome: () -> Unit,
