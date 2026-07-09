8e76ccad1eb0061d8454a0cc7d07d1d4fd54a7c4
apffxhsls <gyeji975@gmail.com>
2026-07-10 06:29:53 +0900

    [mod/#66] 상태바 영역에 색상 넣기

--- presentation/instructor-lesson/src/main/java/com/ssing/presentation/instructorlessondetail/screen/LessonDetailBeforeScreen.kt
+++ presentation/instructor-lesson/src/main/java/com/ssing/presentation/instructorlessondetail/screen/LessonDetailBeforeScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.input.rememberTextFieldState
@@ -77,6 +78,9 @@ internal fun LessonDetailBeforeScreen(
             title = "강습 상세",
             onBack = { onBack() },
             backgroundColor = Blue50,
+            modifier = Modifier
+                .background(Blue50)
+                .statusBarsPadding(),
         )
         Box(
             modifier = modifier
