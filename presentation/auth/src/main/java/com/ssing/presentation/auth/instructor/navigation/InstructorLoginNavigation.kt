9f1d763114b2e78210dd7a4f2e7ee99abb507d11
yeseul <joyri0202@gmail.com>
2026-07-10 01:34:07 +0900

    [del/#88] 불필요한 import 삭제

--- presentation/auth/src/main/java/com/ssing/presentation/auth/instructor/navigation/InstructorLoginNavigation.kt
+++ presentation/auth/src/main/java/com/ssing/presentation/auth/instructor/navigation/InstructorLoginNavigation.kt
@@ -1,8 +1,5 @@
 package com.ssing.presentation.auth.instructor.navigation
 
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.ssing.presentation.auth.instructor.InstructorLoginRoute
@@ -12,7 +9,6 @@ import kotlinx.serialization.Serializable
 data object InstructorLogin
 
 fun NavGraphBuilder.instructorAuthNavGraph(
-    paddingValues: PaddingValues,
     navigateToHome: () -> Unit,
 ) {
     composable<InstructorLogin> {
