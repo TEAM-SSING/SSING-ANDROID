ae77aaa0a4a5049c0f98875904ed37feddfc3284
doorimng <doorimng@gmail.com>
2026-07-10 04:27:01 +0900

    [fix/#81] MainTabRoute -> Route 변경

--- presentation/auth/src/main/java/com/ssing/presentation/auth/consumer/navigation/ConsumerAuthNavigation.kt
+++ presentation/auth/src/main/java/com/ssing/presentation/auth/consumer/navigation/ConsumerAuthNavigation.kt
@@ -5,12 +5,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import com.ssing.core.ui.navigation.MainTabRoute
+import com.ssing.core.ui.navigation.Route
 import com.ssing.presentation.auth.consumer.ConsumerLoginRoute
 import kotlinx.serialization.Serializable
 
 @Serializable
-data object ConsumerLogin : MainTabRoute
+data object ConsumerLogin : Route
 
 fun NavGraphBuilder.consumerAuthNavGraph(
     paddingValues: PaddingValues,
