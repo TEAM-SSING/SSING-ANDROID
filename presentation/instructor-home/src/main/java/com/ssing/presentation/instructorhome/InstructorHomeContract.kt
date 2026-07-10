e665c1e71ad51df520d5f7b1ac4f5c926b6d2124
doorimng <doorimng@gmail.com>
2026-07-11 01:31:07 +0900

    [fix/#109] model 작성

--- presentation/instructor-home/src/main/java/com/ssing/presentation/instructorhome/InstructorHomeContract.kt
+++ presentation/instructor-home/src/main/java/com/ssing/presentation/instructorhome/InstructorHomeContract.kt
@@ -26,29 +26,3 @@ internal interface InstructorHomeContract {
         data class NavigateToLessonDetail(val lessonId: Long) : Effect
     }
 }
-
-enum class Grade (
-    val label: String,
-    @get:DrawableRes val icon: Int
-) {
-    GRADE1(
-        label = "Grade1",
-        icon = R.drawable.img_grade1_badge
-    ),
-    GRADE2(
-        label = "Grade2",
-        icon = R.drawable.img_grade2_badge
-    ),
-    GRADE3(
-        label = "Grade3",
-        icon = R.drawable.img_grade3_badge
-    ),
-    GRADE4(
-        label = "Grade4",
-        icon = R.drawable.img_grade4_badge
-    ),
-    GRADE5(
-        label = "Grade5",
-        icon = R.drawable.img_grade5_badge
-    ),
-}
