d3c2c772fd372ae7efd0584718775e1abb20deff
apffxhsls <gyeji975@gmail.com>
2026-07-10 14:43:17 +0900

    [feat/#103] DataSource, DataSourceImpl 구현

--- data/src/main/java/com/ssing/data/lesson/remote/datasource/api/StartConfirmationDataSource.kt
+++ data/src/main/java/com/ssing/data/lesson/remote/datasource/api/StartConfirmationDataSource.kt
@@ -1,4 +1,9 @@
 package com.ssing.data.lesson.remote.datasource.api
 
+import com.ssing.core.network.model.BaseResponse
+import com.ssing.data.lesson.remote.dto.response.StartConfirmationResponse
+
 interface StartConfirmationDataSource {
+    suspend fun startConfirmation(lessonId: Long):
+            BaseResponse<StartConfirmationResponse>
 }
--- data/src/main/java/com/ssing/data/lesson/remote/datasource/impl/StartConfirmationDataSourceImpl.kt
+++ data/src/main/java/com/ssing/data/lesson/remote/datasource/impl/StartConfirmationDataSourceImpl.kt
@@ -1,4 +1,13 @@
 package com.ssing.data.lesson.remote.datasource.impl
 
-class StartConfirmationDataSourceImpl {
+import com.ssing.data.lesson.remote.datasource.api.StartConfirmationDataSource
+import com.ssing.data.lesson.remote.service.StartConfirmationService
+import javax.inject.Inject
+
+internal class StartConfirmationDataSourceImpl @Inject constructor(
+    private val service: StartConfirmationService,
+) : StartConfirmationDataSource {
+
+    override suspend fun startConfirmation(lessonId: Long) =
+        service.startConfirmation(lessonId)
 }
