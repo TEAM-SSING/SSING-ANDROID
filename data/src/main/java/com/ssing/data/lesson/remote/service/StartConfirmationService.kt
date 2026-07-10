a81bf20f0fe99aaa05632ebd428f83783e4d7766
apffxhsls <gyeji975@gmail.com>
2026-07-10 14:42:47 +0900

    [add/#103] @Path 추가

--- data/src/main/java/com/ssing/data/lesson/remote/service/StartConfirmationService.kt
+++ data/src/main/java/com/ssing/data/lesson/remote/service/StartConfirmationService.kt
@@ -5,10 +5,12 @@ import com.ssing.data.lesson.remote.dto.request.StartConfirmationRequest
 import com.ssing.data.lesson.remote.dto.response.StartConfirmationResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 internal interface StartConfirmationService {
     @POST("api/v1/lessons/{lessonId}/start-confirmation")
     suspend fun startConfirmation(
+        @Path("lessonId") lessonId: Long,
         @Body request: StartConfirmationRequest,
     ): BaseResponse<StartConfirmationResponse>
 }
