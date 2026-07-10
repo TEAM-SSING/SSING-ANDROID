c032066544e15ecac3ee929356f4f8ae1a6bde24
apffxhsls <gyeji975@gmail.com>
2026-07-10 22:48:12 +0900

    [chore/#66] Loading 빼고 기본 화면 데이터 임시로 넣어줌

--- presentation/instructor-lesson/src/main/java/com/ssing/presentation/instructorlessondetail/LessonDetailContract.kt
+++ presentation/instructor-lesson/src/main/java/com/ssing/presentation/instructorlessondetail/LessonDetailContract.kt
@@ -6,12 +6,40 @@ import com.ssing.presentation.instructorlessondetail.model.LessonDetailCanceledU
 import com.ssing.presentation.instructorlessondetail.model.LessonDetailCompletedUiModel
 import com.ssing.presentation.instructorlessondetail.model.LessonDetailOngoingUiModel
 import com.ssing.presentation.instructorlessondetail.screen.CancelReasonState
+import com.ssing.presentation.instructorlessondetail.screen.TeamParticipantsInfo
+import kotlinx.collections.immutable.persistentListOf
 
 internal interface LessonDetailContract {
 
     @Immutable
     data class State(
-        val phase: LessonDetailPhase = LessonDetailPhase.Loading,
+        val phase: LessonDetailPhase = LessonDetailPhase.LessonDetailBefore(            before = LessonDetailBeforeUiModel(
+            isInstructorReady = false,
+            participantReadyCount = 2,
+            participantTotalCount = 5,
+            tags = persistentListOf("스노보드", "자격증이 있어요"),
+            classTitle = "김OO님 팀, 홍지민님 팀 총 5명",
+            location = "OOO 리조트",
+            duration = "0시간",
+            price = 0,
+            teams = persistentListOf(
+                TeamParticipantsInfo(
+                    teamNickname = "김OO",
+                    teamCount = 0,
+                    participants = persistentListOf("38세 남", "12세 여", "9세 남"),
+                    price = 0,
+                    isReady = true,
+                ),
+                TeamParticipantsInfo(
+                    teamNickname = "김OO",
+                    teamCount = 0,
+                    participants = persistentListOf("38세 남", "12세 여", "9세 남"),
+                    price = 0,
+                    isReady = false,
+                ),
+            ),
+        ),
+        ),
         val showReadyDialog: Boolean = false,
         val showLessonEndDialog: Boolean = false,
         val cancelReasonState: CancelReasonState = CancelReasonState(),
