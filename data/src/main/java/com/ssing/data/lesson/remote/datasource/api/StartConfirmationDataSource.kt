3a15e7e3e38459af7311e7a448040315baa3a4a7
apffxhsls <gyeji975@gmail.com>
2026-07-10 14:50:29 +0900

    [add/#103] DataSource, DataSourceImpl request 추가

--- data/src/main/java/com/ssing/data/lesson/remote/datasource/api/StartConfirmationDataSource.kt
+++ data/src/main/java/com/ssing/data/lesson/remote/datasource/api/StartConfirmationDataSource.kt
@@ -1,9 +1,12 @@
 package com.ssing.data.lesson.remote.datasource.api
 
 import com.ssing.core.network.model.BaseResponse
+import com.ssing.data.lesson.remote.dto.request.StartConfirmationRequest
 import com.ssing.data.lesson.remote.dto.response.StartConfirmationResponse
 
 interface StartConfirmationDataSource {
-    suspend fun startConfirmation(lessonId: Long):
+    suspend fun startConfirmation(
+        lessonId: Long,
+        request: StartConfirmationRequest,):
             BaseResponse<StartConfirmationResponse>
 }
