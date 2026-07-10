e6775ad9c3b8509f5e49d18a9f3054fbe5c3eb98
yeseul <joyri0202@gmail.com>
2026-07-11 02:29:58 +0900

    [feat/#104] ConsumerLessonNavigation 추가

---
+++ presentation/consumer-lesson/src/main/java/com/ssing/presentation/consumerlesson/navigation/ConsumerLessonNavigation.kt
@@ -0,0 +1,26 @@
+package com.ssing.presentation.consumerlesson.navigation
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.ssing.core.ui.navigation.Route
+import com.ssing.presentation.consumerlesson.ConsumerLessonRoute
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object ConsumerLesson : Route
+
+
+fun NavGraphBuilder.consumerLessonNavGraph(
+    paddingValues: PaddingValues,
+    navigateToHome: () -> Unit,
+) {
+    composable<ConsumerLesson> {
+        ConsumerLessonRoute(
+            modifier = Modifier.padding(paddingValues),
+            navigateToHome = navigateToHome,
+        )
+    }
+}
