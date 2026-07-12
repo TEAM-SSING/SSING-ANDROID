c9ea1b8aa0dcfd52ff5348c504fb36e36dd3d1c8
dodo <eg4rang@naver.com>
2026-07-12 21:29:53 +0900

    [fix/#118] SsingMatchingDetailCard borderColor 호이스팅

--- core/ui/src/main/java/com/ssing/core/ui/common/component/SsingMatchingDetailCard.kt
+++ core/ui/src/main/java/com/ssing/core/ui/common/component/SsingMatchingDetailCard.kt
@@ -65,10 +65,11 @@ fun SsingMatchingDetailCard(
     price: Int? = null,
     equipmentStatus: String = "",
     title: String? = null,
+    borderColor: Color = Blue200,
 ) {
     Column(
         modifier = modifier
-            .border(width = 1.dp, color = Blue200, shape = RoundedCornerShape(12.dp))
+            .border(width = 1.dp, color = borderColor, shape = RoundedCornerShape(12.dp))
             .clip(RoundedCornerShape(12.dp))
             .background(color = SSINGTheme.colors.backgroundNormal)
             .padding(16.dp),
