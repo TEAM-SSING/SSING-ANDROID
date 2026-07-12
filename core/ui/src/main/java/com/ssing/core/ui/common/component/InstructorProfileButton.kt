001d284c202230f484630572d16ea94144d18c49
dodo <eg4rang@naver.com>
2026-07-13 02:48:34 +0900

    [feat/#130] 강사 프로필 디폴트 이미지 적용

--- core/ui/src/main/java/com/ssing/core/ui/common/component/InstructorProfileButton.kt
+++ core/ui/src/main/java/com/ssing/core/ui/common/component/InstructorProfileButton.kt
@@ -1,5 +1,6 @@
 package com.ssing.core.ui.common.component
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -16,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -75,8 +77,9 @@ private fun ProfileSection(
         horizontalArrangement = Arrangement.spacedBy(10.dp),
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        UrlImage(
-            url = imageUrl,
+        Image(
+            painter = painterResource(R.drawable.img_default_profile),
+            contentDescription = null,
             modifier = Modifier
                 .clip(CircleShape)
                 .width(44.dp)
