20c80f27846c7a536a3af5540f1bd96aac9c38a8
doorimng <doorimng@gmail.com>
2026-07-13 01:47:08 +0900

    [del/#106] 불필요한 import문 삭제

--- presentation/consumer-home/src/main/java/com/ssing/presentation/consumerhome/navigation/ConsumerHomeNavigation.kt
+++ presentation/consumer-home/src/main/java/com/ssing/presentation/consumerhome/navigation/ConsumerHomeNavigation.kt
@@ -1,8 +1,6 @@
 package com.ssing.presentation.consumerhome.navigation
 
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.ssing.core.ui.navigation.MainTabRoute
