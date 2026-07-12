71711ed45dcbe643555e43d687a7375eaa7c4de4
doorimng <doorimng@gmail.com>
2026-07-12 20:58:04 +0900

    [feat/#106] 네비게이션 연결

--- app/consumer/src/main/java/com/ssing/consumer/ConsumerMainNavHost.kt
+++ app/consumer/src/main/java/com/ssing/consumer/ConsumerMainNavHost.kt
@@ -10,7 +10,7 @@ import com.ssing.presentation.auth.consumer.navigation.consumerAuthNavGraph
 import com.ssing.core.ui.navigation.SsingNavHost
 import com.ssing.presentation.consumerhome.navigation.ConsumerHome
 import com.ssing.presentation.consumerhome.navigation.consumerHomeNavGraph
-import com.ssing.presentation.consumermatching.navigation.ConsumerMatchingResult
+import com.ssing.presentation.consumermatching.navigation.ConsumerMatchingCondition
 import com.ssing.presentation.consumermatching.navigation.consumerMatchingNavGraph
 import com.ssing.presentation.notification.navigation.notificationNavGraph
 
@@ -28,13 +28,13 @@ internal fun ConsumerMainNavHost(
         consumerHomeNavGraph(
             navigateToLessonDetail = {
                 navController.navigate(
-                    route = ConsumerHome,
+                    route = {}, // TODO: 강습 상세 뷰 연결
                     navOptions = navController.clearBackStackNavOptions(),
                 )
             },
             navigateToMatching = {
                 navController.navigate(
-                    route = ConsumerMatchingResult,
+                    route = ConsumerMatchingCondition,
                     navOptions = navController.clearBackStackNavOptions(),
                 )
             },
