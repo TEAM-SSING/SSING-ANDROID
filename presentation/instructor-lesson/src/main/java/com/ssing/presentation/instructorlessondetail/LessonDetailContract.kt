391b0aea0ac9aca25cf5aaa211826a992049ea63
apffxhsls <gyeji975@gmail.com>
2026-07-13 00:09:11 +0900

    [add/#103] lessonId 필드 추가

--- presentation/instructor-lesson/src/main/java/com/ssing/presentation/instructorlessondetail/LessonDetailContract.kt
+++ presentation/instructor-lesson/src/main/java/com/ssing/presentation/instructorlessondetail/LessonDetailContract.kt
@@ -22,6 +22,7 @@ internal interface LessonDetailContract {
             location = "OOO 리조트",
             duration = "0시간",
             price = 0,
+            lessonId = 1023658,
             teams = persistentListOf(
                 TeamParticipantsInfo(
                     teamNickname = "김OO",
