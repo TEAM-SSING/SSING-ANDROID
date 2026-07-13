b85a235f4589346f0cbcb3f072bf34a2f2d3f981
doorimng <doorimng@gmail.com>
2026-07-13 22:34:39 +0900

    [fix/#139] 데이터소스 수정

--- data/src/main/java/com/ssing/data/consumerhome/remote/datasource/api/ConsumerHomeRemoteDataSource.kt
+++ data/src/main/java/com/ssing/data/consumerhome/remote/datasource/api/ConsumerHomeRemoteDataSource.kt
@@ -3,6 +3,6 @@ package com.ssing.data.consumerhome.remote.datasource.api
 import com.ssing.core.network.model.BaseResponse
 import com.ssing.data.consumerhome.remote.dto.response.ConsumerHomeResponse
 
-internal interface ConsumerHomeDataSource{
+internal interface ConsumerHomeRemoteDataSource{
     suspend fun getConsumerHome(): BaseResponse<ConsumerHomeResponse>
 }
--- data/src/main/java/com/ssing/data/consumerhome/remote/datasource/impl/ConsumerHomeRemoteDataSourceImpl.kt
+++ data/src/main/java/com/ssing/data/consumerhome/remote/datasource/impl/ConsumerHomeRemoteDataSourceImpl.kt
@@ -1,13 +1,15 @@
 package com.ssing.data.consumerhome.remote.datasource.impl
 
 import com.ssing.core.network.model.BaseResponse
-import com.ssing.data.consumerhome.remote.datasource.api.ConsumerHomeDataSource
+import com.ssing.data.consumerhome.remote.datasource.api.ConsumerHomeRemoteDataSource
 import com.ssing.data.consumerhome.remote.dto.response.ConsumerHomeResponse
 import com.ssing.data.consumerhome.remote.service.ConsumerHomeService
 import javax.inject.Inject
 
-internal class ConsumerHomeDataSourceImpl @Inject constructor(
+internal class ConsumerHomeRemoteDataSourceImpl @Inject constructor(
     private val service: ConsumerHomeService,
-) : ConsumerHomeDataSource {
-    override suspend fun getConsumerHome(): BaseResponse<ConsumerHomeResponse> = service.getConsumerHome()
+) : ConsumerHomeRemoteDataSource {
+
+    override suspend fun getConsumerHome():
+            BaseResponse<ConsumerHomeResponse> = service.getConsumerHome()
 }
