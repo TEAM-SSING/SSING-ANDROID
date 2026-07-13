edc7547f35ad64d1dd15483b3a5adfc2039ce1b1
apffxhsls <gyeji975@gmail.com>
2026-07-13 18:33:14 +0900

    [del/#107] 안 쓰는 import 삭제

--- data/src/main/java/com/ssing/data/instructorlessondetail/remote/datasource/api/InstructorLessonDetailDataSource.kt
+++ data/src/main/java/com/ssing/data/instructorlessondetail/remote/datasource/api/InstructorLessonDetailDataSource.kt
@@ -1,7 +1,6 @@
 package com.ssing.data.instructorlessondetail.remote.datasource.api
 
 import com.ssing.core.network.model.BaseResponse
-import com.ssing.data.instructorlessondetail.remote.dto.request.InstructorLessonDetailRequest
 import com.ssing.data.instructorlessondetail.remote.dto.response.InstructorLessonDetailResponse
 
 internal interface InstructorLessonDetailDataSource {
