8d71142eee2dd8b986944d5bc5b761c28eb8d673
yeseul <joyri0202@gmail.com>
2026-07-14 01:34:26 +0900

    [mod/#138] Module 생성

---
+++ data/src/main/java/com/ssing/data/consumerlesson/di/ConsumerLessonDetailServiceModule.kt
@@ -0,0 +1,21 @@
+package com.ssing.data.consumerlesson.di
+
+import com.ssing.data.consumerlesson.remote.service.ConsumerLessonDetailService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+import kotlin.jvm.java
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object ConsumerLessonDetailServiceModule {
+
+    @Provides
+    @Singleton
+    fun provideConsumerLessonDetailService(
+        retrofit: Retrofit,
+    ): ConsumerLessonDetailService = retrofit.create(ConsumerLessonDetailService::class.java)
+}
