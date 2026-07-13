7d9d72a12f35220b63ef55fb1cb46f4e708be3aa
dodo <eg4rang@naver.com>
2026-07-14 02:21:13 +0900

    [feat/#146] LessonSocketDataSource 구현

---
+++ data/src/main/java/com/ssing/data/lesson/common/remote/datasource/api/LessonSocketDataSource.kt
@@ -0,0 +1,14 @@
+package com.ssing.data.lesson.common.remote.datasource.api
+
+import com.ssing.core.network.socket.SocketState
+import com.ssing.core.network.socket.lesson.LessonEnvelope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.serialization.json.JsonElement
+
+internal interface LessonSocketDataSource {
+    val event: Flow<LessonEnvelope<JsonElement>>
+    val socketState: StateFlow<SocketState>
+    fun connect()
+    suspend fun disconnect()
+}
