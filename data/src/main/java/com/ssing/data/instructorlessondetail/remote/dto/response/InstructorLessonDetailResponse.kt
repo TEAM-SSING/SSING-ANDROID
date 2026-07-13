2c1f730516c0f5f47d4d80e788401faf3ff6486e
apffxhsls <gyeji975@gmail.com>
2026-07-13 16:58:52 +0900

    [feat/#107] InstructorLessonDetailResponse 구현 - 분기 처리

---
+++ data/src/main/java/com/ssing/data/instructorlessondetail/remote/dto/response/InstructorLessonDetailResponse.kt
@@ -0,0 +1,24 @@
+package com.ssing.data.instructorlessondetail.remote.dto.response
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonContentPolymorphicSerializer
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
+
+@Serializable(with = InstructorLessonDetailResponseSerializer::class)
+internal sealed interface InstructorLessonDetailResponse
+
+internal object InstructorLessonDetailResponseSerializer :
+    JsonContentPolymorphicSerializer<InstructorLessonDetailResponse>(
+        InstructorLessonDetailResponse::class
+    ) {
+    override fun selectDeserializer(element: JsonElement) =
+        when (element.jsonObject["lessonStatus"]?.jsonPrimitive?.content) {
+            "CONFIRMED" -> InstructorLessonDetailBeforeResponse.serializer()
+            "IN_PROGRESS" -> InstructorLessonDetailOngoingResponse.serializer()
+            "COMPLETED" -> InstructorLessonDetailCompletedResponse.serializer()
+            "CANCELED" -> InstructorLessonDetailCanceledResponse.serializer()
+            else -> error("Unknown lessonStatus: ${element.jsonObject["lessonStatus"]}")
+        }
+}
