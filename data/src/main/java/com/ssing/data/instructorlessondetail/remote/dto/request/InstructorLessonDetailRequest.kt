76403e6d7ee0fff7e5f151c3e7bbfc465b0d07d6
apffxhsls <gyeji975@gmail.com>
2026-07-13 16:00:44 +0900

    [feat/#107] InstructorLessonDetailRequest 구현

---
+++ data/src/main/java/com/ssing/data/instructorlessondetail/remote/dto/request/InstructorLessonDetailRequest.kt
@@ -0,0 +1,10 @@
+package com.ssing.data.instructorlessondetail.remote.dto.request
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+internal data class InstructorLessonDetailRequest(
+    @SerialName("lessonId")
+    val lessonId: Int,
+)
