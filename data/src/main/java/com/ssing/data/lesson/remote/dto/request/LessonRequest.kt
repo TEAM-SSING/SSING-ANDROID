fbdc611330930ef1ec6b7f1a9f876bc969c01fd2
apffxhsls <gyeji975@gmail.com>
2026-07-13 20:27:21 +0900

    [add/#103] internal 추가

--- data/src/main/java/com/ssing/data/lesson/remote/dto/request/LessonRequest.kt
+++ data/src/main/java/com/ssing/data/lesson/remote/dto/request/LessonRequest.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class LessonRequest(
+internal data class LessonRequest(
     @SerialName("lessonId")
     val lessonId: Long,
 )
--- data/src/main/java/com/ssing/data/lesson/remote/dto/response/LessonResponse.kt
+++ data/src/main/java/com/ssing/data/lesson/remote/dto/response/LessonResponse.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class LessonResponse(
+internal data class LessonResponse(
     @SerialName("lessonId")
     val lessonId: Long,
     @SerialName("lessonStatus")
