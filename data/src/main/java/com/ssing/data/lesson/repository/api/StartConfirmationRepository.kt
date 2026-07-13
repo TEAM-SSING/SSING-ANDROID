10dd802b0fa036577fa22c2e5894bf3e7a0f2e4d
apffxhsls <gyeji975@gmail.com>
2026-07-13 10:23:27 +0900

    [del/#103] 안 쓰는 import 삭제

--- data/src/main/java/com/ssing/data/lesson/repository/api/StartConfirmationRepository.kt
+++ data/src/main/java/com/ssing/data/lesson/repository/api/StartConfirmationRepository.kt
@@ -1,7 +1,5 @@
 package com.ssing.data.lesson.repository.api
 
-import com.ssing.data.lesson.remote.dto.request.StartConfirmationRequest
-
 interface StartConfirmationRepository {
     suspend fun confirmLessonStart(lessonId: Long): Result<Unit>
 }
