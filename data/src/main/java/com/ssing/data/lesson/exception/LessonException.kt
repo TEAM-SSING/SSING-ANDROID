761ed9f945e71dcfd299e5930dd1bcdbdb7dc743
apffxhsls <gyeji975@gmail.com>
2026-07-13 20:22:15 +0900

    [del/#103] LessonException 삭제

--- data/src/main/java/com/ssing/data/lesson/exception/LessonException.kt
+++
@@ -1,9 +0,0 @@
-package com.ssing.data.lesson.exception
-
-import com.ssing.core.network.exception.BusinessException
-
-sealed class LessonException(
-    serverCode: String,
-    message: String,
-    requestId: String,
-) : BusinessException(serverCode, message, requestId)
