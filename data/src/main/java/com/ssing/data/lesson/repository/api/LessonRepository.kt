415ae06a33a587dd874146483ccfbc04211aab73
apffxhsls <gyeji975@gmail.com>
2026-07-13 23:28:24 +0900

    [mod/#107] 반환값 변경

--- data/src/main/java/com/ssing/data/lesson/repository/api/LessonRepository.kt
+++ data/src/main/java/com/ssing/data/lesson/repository/api/LessonRepository.kt
@@ -1,9 +1,11 @@
 package com.ssing.data.lesson.repository.api
 
+import com.ssing.data.lesson.remote.dto.response.InstructorLessonDetailResponse
+
 interface LessonRepository {
     suspend fun lessonStart(lessonId: Long): Result<Unit>
 }
 
 interface InstructorLessonDetailRepository {
-    suspend fun instructorLessonDetail(lessonId: Long): Result<Unit>
+    suspend fun instructorLessonDetail(lessonId: Long): Result<InstructorLessonDetailResponse>
 }
--- data/src/main/java/com/ssing/data/lesson/repository/impl/LessonRepositoryImpl.kt
+++ data/src/main/java/com/ssing/data/lesson/repository/impl/LessonRepositoryImpl.kt
@@ -4,6 +4,7 @@ import com.ssing.core.network.util.ApiResponseHandler
 import com.ssing.data.lesson.remote.datasource.api.InstructorLessonDetailDataSource
 import com.ssing.data.lesson.remote.datasource.api.LessonDataSource
 import com.ssing.data.lesson.remote.dto.request.LessonRequest
+import com.ssing.data.lesson.remote.dto.response.InstructorLessonDetailResponse
 import com.ssing.data.lesson.repository.api.InstructorLessonDetailRepository
 import com.ssing.data.lesson.repository.api.LessonRepository
 import javax.inject.Inject
@@ -26,10 +27,11 @@ internal class InstructorLessonDetailRepositoryImpl @Inject constructor(
     private val apiResponseHandler: ApiResponseHandler,
     private val dataSource: InstructorLessonDetailDataSource,
 ) : InstructorLessonDetailRepository {
-    override suspend fun instructorLessonDetail(lessonId: Long): Result<Unit> =
+    override suspend fun instructorLessonDetail(lessonId: Long): Result<InstructorLessonDetailResponse> =
         apiResponseHandler.safeApiCall {
             dataSource.instructorLessonDetail(
                 lessonId = lessonId,
             )
-        }.map { }
+        }
 }
+
