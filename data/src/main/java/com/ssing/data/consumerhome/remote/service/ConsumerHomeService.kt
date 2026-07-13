a9db7ce975e21c73c62287205994122da33d5db5
doorimng <doorimng@gmail.com>
2026-07-13 20:18:32 +0900

    [feat/#139] Service 작성

--- data/src/main/java/com/ssing/data/consumerhome/remote/service/ConsumerHomeService.kt
+++ data/src/main/java/com/ssing/data/consumerhome/remote/service/ConsumerHomeService.kt
@@ -1,2 +1,10 @@
 package com.ssing.data.consumerhome.remote.service
 
+import com.ssing.core.network.model.BaseResponse
+import com.ssing.data.consumerhome.remote.dto.response.ConsumerHomeResponse
+import retrofit2.http.GET
+
+internal interface ConsumerHomeService {
+    @GET("api/v1/consumer/home")
+    suspend fun getConsumerHome(): BaseResponse<ConsumerHomeResponse>
+}
