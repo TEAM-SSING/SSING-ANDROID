2f6219644ca154219302f8dfe658bcf7e802329c
doorimng <doorimng@gmail.com>
2026-07-13 23:34:16 +0900

    [fix/#139] Response 내부 타입 변경

--- data/src/main/java/com/ssing/data/consumerhome/remote/dto/response/LessonCardResponse.kt
+++ data/src/main/java/com/ssing/data/consumerhome/remote/dto/response/LessonCardResponse.kt
@@ -1,15 +1,17 @@
 package com.ssing.data.consumerhome.remote.dto.response
 
+import com.ssing.data.consumerhome.model.DisplayStatus
+import com.ssing.data.consumerhome.model.Sports
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-internal data class LessonCardsResponse(
+internal data class LessonCardResponse(
     @SerialName("lessonId") val lessonId: Long,
     @SerialName("remainingDays") val remainingDays: Int,
-    @SerialName("displayStatus") val displayStatus: String,
+    @SerialName("displayStatus") val displayStatus: DisplayStatus,
     @SerialName("title") val title: String,
-    @SerialName("sport") val sport: String,
+    @SerialName("sport") val sport: Sports,
     @SerialName("scheduledAt") val scheduledAt: String,
     @SerialName("resort") val resort: ResortResponse,
 )
