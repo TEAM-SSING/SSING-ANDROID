fb9910377067611bf2ceb3d9482ae9339f827115
doorimng <doorimng@gmail.com>
2026-07-13 22:33:47 +0900

    [feat/#139] 모델 작성

---
+++ data/src/main/java/com/ssing/data/consumerhome/model/ConsumerHome.kt
@@ -0,0 +1,31 @@
+package com.ssing.data.consumerhome.model
+
+data class ConsumerHome(
+    val lessonCards: LessonCards,
+    val matchingPeopleCount: Long,
+    val hasUnreadNotification: Boolean,
+)
+
+data class LessonCards(
+    val lessonId: Long,
+    val remainingDays: Int,
+    val displayStatus: DisplayStatus,
+    val title: String,
+    val sport: Sports,
+    val resort: Resort,
+)
+
+data class Resort(
+    val code: String,
+    val displayName: String,
+)
+
+enum class DisplayStatus(val label: String,){
+    CONFIRMED("CONFIRMED"),
+    IN_PROGRESS("IN_PROGRESS"),
+}
+
+enum class Sports(val label: String){
+    SKI("SKI"),
+    SNOWBOARD("SNOWBOARD"),
+}
