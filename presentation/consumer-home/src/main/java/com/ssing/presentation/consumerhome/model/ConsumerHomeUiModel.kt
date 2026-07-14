ec8cf0f9636934d9dbcce21f11afc769ca02ad18
doorimng <doorimng@gmail.com>
2026-07-15 01:45:01 +0900

    [del/#139] 불필요한 enum class 삭제

--- presentation/consumer-home/src/main/java/com/ssing/presentation/consumerhome/model/ConsumerHomeUiModel.kt
+++ presentation/consumer-home/src/main/java/com/ssing/presentation/consumerhome/model/ConsumerHomeUiModel.kt
@@ -1,10 +1,5 @@
 package com.ssing.presentation.consumerhome.model
 
-internal enum class Sports(val label: String) {
-    SKI("SKI"),
-    SNOWBOARD("SNOWBOARD")
-}
-
 internal enum class DisplayStatus(val label: String) {
     CONFIRMED("CONFIRMED"),
     IN_PROGRESS("IN_PROGRESS")
