363ad51d3464d6c549bb15fdb19028c2ef5ac17a
oilbeaneda <20221444@sungshin.ac.kr>
2026-07-14 15:24:11 +0900

    [feat/#151] 서비스에  getMatchingOffers  추가

--- data/src/main/java/com/ssing/data/matching/instructormatching/remote/service/InstructorMatchingService.kt
+++ data/src/main/java/com/ssing/data/matching/instructormatching/remote/service/InstructorMatchingService.kt
@@ -2,9 +2,17 @@ package com.ssing.data.matching.instructormatching.remote.service
 
 import com.ssing.core.network.model.BaseResponse
 import com.ssing.data.matching.instructormatching.remote.dto.response.InstructorMatchingExposureResponse
+import com.ssing.data.matching.instructormatching.remote.dto.response.InstructorMatchingOffersResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 internal interface InstructorMatchingService {
     @GET("/api/v1/instructor/matching-exposure")
     suspend fun getMatchingExposure(): BaseResponse<InstructorMatchingExposureResponse>
+
+    @GET("/api/v1/instructor/matching-offers")
+    suspend fun getMatchingOffers(
+        @Query("page") page: Int? = null,
+        @Query("size") size: Int? = null,
+    ): BaseResponse<InstructorMatchingOffersResponse>
 }
