d8929fbf7cf658c89afed7fb7cb1b24fd6edc182
doorimng <doorimng@gmail.com>
2026-07-15 02:21:28 +0900

    [fix/#129] 아이콘 색상 및 사이즈 변경

--- core/ui/src/main/java/com/ssing/core/ui/common/component/HomeLessonCardList.kt
+++ core/ui/src/main/java/com/ssing/core/ui/common/component/HomeLessonCardList.kt
@@ -132,7 +132,7 @@ private fun HomeLessonEmptyCard(
             imageVector = ImageVector.vectorResource(R.drawable.ic_empty_reservation_76),
             contentDescription = null,
             tint = Color.Unspecified,
-            modifier = Modifier.size(66.dp),
+            modifier = Modifier.size(76.dp),
         )
     }
 }
@@ -276,7 +276,7 @@ private fun EmptyLessonInfoSection(
         Icon(
             imageVector = ImageVector.vectorResource(R.drawable.ic_plus_large),
             contentDescription = null,
-            tint = SSINGTheme.colors.textAlternative,
+            tint = SSINGTheme.colors.textDisabled,
         )
 
         Spacer(modifier = Modifier.height(6.dp))
