5528704765b9023042b4a98f37dcf8c934036ade
apffxhsls <gyeji975@gmail.com>
2026-07-14 22:51:10 +0900

    [feat/#165] LessonSocketRepository 구현

---
+++ data/src/main/java/com/ssing/data/lesson/repository/api/LessonSocketRepository.kt
@@ -0,0 +1,16 @@
+package com.ssing.data.lesson.repository.api
+
+import com.ssing.core.network.socket.SocketState
+import com.ssing.core.network.socket.lesson.LessonEnvelope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.serialization.json.JsonElement
+
+interface LessonSocketRepository {
+    val event: Flow<LessonEnvelope<JsonElement>>
+    val socketState: StateFlow<SocketState>
+
+    fun connect()
+
+    suspend fun disconnect()
+}
