a173072edb2b6a4f32baba4c611041bf5d48cbad
apffxhsls <gyeji975@gmail.com>
2026-07-14 22:51:21 +0900

    [feat/#165] LessonSocketRepositoryImpl 구현

---
+++ data/src/main/java/com/ssing/data/lesson/repository/impl/LessonSocketRepositoryImpl.kt
@@ -0,0 +1,29 @@
+package com.ssing.data.lesson.repository.impl
+
+import com.ssing.core.network.socket.SocketState
+import com.ssing.core.network.socket.lesson.LessonEnvelope
+import com.ssing.data.lesson.common.remote.datasource.api.LessonSocketDataSource
+import com.ssing.data.lesson.repository.api.LessonSocketRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.serialization.json.JsonElement
+import javax.inject.Inject
+
+internal class LessonSocketRepositoryImpl @Inject constructor(
+    private val dataSource: LessonSocketDataSource,
+) : LessonSocketRepository {
+
+    override val event: Flow<LessonEnvelope<JsonElement>>
+        get() = dataSource.event
+
+    override val socketState: StateFlow<SocketState>
+        get() = dataSource.socketState
+
+    override fun connect() {
+        dataSource.connect()
+    }
+
+    override suspend fun disconnect() {
+        dataSource.disconnect()
+    }
+}
