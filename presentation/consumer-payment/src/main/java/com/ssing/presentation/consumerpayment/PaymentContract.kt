16ecdb8b59ce2398742b1bd4babff048ae9da68b
oilbeaneda <20221444@sungshin.ac.kr>
2026-07-14 21:54:39 +0900

    [chore/#162] 시작됩니다가 아니라 시작

--- presentation/consumer-payment/src/main/java/com/ssing/presentation/consumerpayment/PaymentContract.kt
+++ presentation/consumer-payment/src/main/java/com/ssing/presentation/consumerpayment/PaymentContract.kt
@@ -13,7 +13,7 @@ internal interface PaymentContract {
         val showCancelModal: Boolean = false,
         val nickname: String = "",
         val tags: ImmutableList<String> = persistentListOf(),
-        val classDateTime: String = "강사와 만난 뒤 강습이 시작됩니다",
+        val classDateTime: String = "강사와 만난 뒤 강습 시작",
         val location: String = "",
         val duration: String = "",
         val participant: String = "",
