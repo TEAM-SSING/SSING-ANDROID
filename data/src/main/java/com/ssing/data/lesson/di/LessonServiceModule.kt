b8e7a4b873fb4de88f095243ab8e7dcf90b4920e
apffxhsls <gyeji975@gmail.com>
2026-07-15 03:35:52 +0900

    [fix/#107] 원상복구

--- data/src/main/java/com/ssing/data/lesson/di/LessonServiceModule.kt
+++ data/src/main/java/com/ssing/data/lesson/di/LessonServiceModule.kt
@@ -14,7 +14,6 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 internal object LessonServiceModule {
 
-    @Binds
     @Provides
     @Singleton
     fun provideLessonService(
