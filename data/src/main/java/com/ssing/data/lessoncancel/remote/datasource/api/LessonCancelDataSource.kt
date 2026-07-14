b44713ef676333c365ac57d3745cc8afe5daa814
yeseul <joyri0202@gmail.com>
2026-07-14 22:10:39 +0900

    [mod/#163] DataSource 생성

---
+++ data/src/main/java/com/ssing/data/lessoncancel/remote/datasource/api/LessonCancelDataSource.kt
@@ -0,0 +1,12 @@
+package com.ssing.data.lessoncancel.remote.datasource.api
+
+import com.ssing.core.network.model.BaseResponse
+import com.ssing.data.lessoncancel.remote.dto.response.LessonCancelResponse
+
+internal interface LessonCancelDataSource {
+    suspend fun postLessonCancel(
+        lessonId: Long,
+        cancelReason: String,
+        cancelReasonDetail: String,
+    ): BaseResponse<LessonCancelResponse>
+}
