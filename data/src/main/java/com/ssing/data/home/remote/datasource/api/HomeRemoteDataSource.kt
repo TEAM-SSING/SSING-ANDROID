131f70dda0ae1648346a7893b69a741111440047
doorimng <doorimng@gmail.com>
2026-07-15 22:25:50 +0900

    [feat/#187] datasource 작성

--- data/src/main/java/com/ssing/data/home/remote/datasource/api/HomeRemoteDataSource.kt
+++ data/src/main/java/com/ssing/data/home/remote/datasource/api/HomeRemoteDataSource.kt
@@ -2,7 +2,9 @@ package com.ssing.data.home.remote.datasource.api
 
 import com.ssing.core.network.model.BaseResponse
 import com.ssing.data.home.remote.dto.response.ConsumerHomeResponse
+import com.ssing.data.home.remote.dto.response.InstructorHomeResponse
 
 internal interface HomeRemoteDataSource{
     suspend fun getConsumerHome(): BaseResponse<ConsumerHomeResponse>
+    suspend fun getInstructorHome(): BaseResponse<InstructorHomeResponse>
 }
