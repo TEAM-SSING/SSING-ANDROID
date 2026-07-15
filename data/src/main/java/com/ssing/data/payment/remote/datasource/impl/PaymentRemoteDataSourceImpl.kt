59813f95ed79e66a4b6dc1f943a56e6f36d465b9
doorimng <doorimng@gmail.com>
2026-07-15 17:54:26 +0900

    [feat/#176] datasource 구현

---
+++ data/src/main/java/com/ssing/data/payment/remote/datasource/impl/PaymentRemoteDataSourceImpl.kt
@@ -0,0 +1,22 @@
+package com.ssing.data.payment.remote.datasource.impl
+
+import com.ssing.core.network.model.BaseResponse
+import com.ssing.data.payment.remote.datasource.api.PaymentRemoteDataSource
+import com.ssing.data.payment.remote.dto.request.PaymentRequest
+import com.ssing.data.payment.remote.dto.response.PaymentResponse
+import com.ssing.data.payment.remote.service.PaymentService
+import javax.inject.Inject
+
+internal class PaymentRemoteDataSourceImpl @Inject constructor(
+    private val paymentService: PaymentService,
+) : PaymentRemoteDataSource {
+
+    override suspend fun postPaymentRequest(
+        matchingRequestId: Long,
+        request: PaymentRequest,
+    ): BaseResponse<PaymentResponse> =
+        paymentService.postPaymentService(
+            matchingRequestId = matchingRequestId,
+            request = request
+        )
+}
