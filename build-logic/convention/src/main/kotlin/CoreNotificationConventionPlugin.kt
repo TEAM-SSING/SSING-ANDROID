e2e1abf1a1feb590989631f83c375c26d9f852b4
oilbeaneda <20221444@sungshin.ac.kr>
2026-07-16 00:39:59 +0900

    [feat/#105] CoreNotificationConventionPlugin 추가

--- build-logic/convention/src/main/kotlin/CoreNotificationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/CoreNotificationConventionPlugin.kt
@@ -10,6 +10,7 @@ class CoreNotificationConventionPlugin : Plugin<Project> {
             with(pluginManager) {
                 apply("com.google.dagger.hilt.android")
                 apply("com.google.devtools.ksp")
+                apply("org.jetbrains.kotlin.plugin.serialization")
             }
             dependencies {
                 add("implementation", project(":core:network"))
@@ -17,6 +18,7 @@ class CoreNotificationConventionPlugin : Plugin<Project> {
                 add("ksp", libs.findLibrary("hilt.compiler").get())
                 add("implementation", libs.findLibrary("retrofit.core").get())
                 add("implementation", libs.findLibrary("retrofit.kotlin.serialization").get())
+                add("implementation", libs.findLibrary("kotlinx.serialization.json").get())
                 add("implementation", libs.findLibrary("timber").get())
                 add("implementation", libs.findLibrary("androidx.core.ktx").get())
                 add("implementation", platform(libs.findLibrary("firebase.bom").get()))
