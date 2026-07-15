62ead59a7cac9534d99e5b35a86ee73b932c644e
doorimng <doorimng@gmail.com>
2026-07-15 18:11:49 +0900

    [del/#176] 불필요한 import문 삭제

--- data/src/main/java/com/ssing/data/payment/remote/service/PaymentService.kt
+++ data/src/main/java/com/ssing/data/payment/remote/service/PaymentService.kt
@@ -1,9 +1,7 @@
 package com.ssing.data.payment.remote.service
 
 import com.ssing.core.network.model.BaseResponse
-import com.ssing.data.payment.remote.dto.request.PaymentRequest
 import com.ssing.data.payment.remote.dto.response.PaymentResponse
-import retrofit2.http.Body
 import retrofit2.http.POST
 import retrofit2.http.Path
 
@@ -11,6 +9,5 @@ internal interface PaymentService {
     @POST("/api/v1/consumer/matching-requests/{matchingRequestId}/payment")
     suspend fun postPaymentService(
         @Path("matchingRequestId") matchingRequestId: Long,
-        @Body request: PaymentRequest,
     ): BaseResponse<PaymentResponse>
 }
