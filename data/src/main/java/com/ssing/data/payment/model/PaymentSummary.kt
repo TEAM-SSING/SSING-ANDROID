f9965553696f61f8814c8cbc851a7c2f590b1344
doorimng <doorimng@gmail.com>
2026-07-15 17:55:48 +0900

    [feat/#176] model 작성

---
+++ data/src/main/java/com/ssing/data/payment/model/PaymentSummary.kt
@@ -0,0 +1,13 @@
+package com.ssing.data.payment.model
+
+data class PaymentSummary(
+    val matchingRequestId: Long,
+    val matchingStatus: String,
+    val paymentStatus: String,
+    val groupId: Long,
+    val groupStatus: String,
+    val paidCount: Int,
+    val requiredCount: Int,
+    val lessonId: Long,
+    val expiresAt: String,
+)
