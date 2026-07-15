7aa19db15658b241fcfac5a3a3c6a62b9a273906
yeseul <joyri0202@gmail.com>
2026-07-16 06:05:30 +0900

    [mod/#189] service 응답 형태 변경

--- data/src/main/java/com/ssing/data/lesson/common/remote/service/LessonService.kt
+++ data/src/main/java/com/ssing/data/lesson/common/remote/service/LessonService.kt
@@ -1,8 +1,10 @@
 package com.ssing.data.lesson.common.remote.service
 
 import com.ssing.core.network.model.BaseResponse
-import com.ssing.data.lesson.common.remote.dto.response.LessonResponse
-import com.ssing.data.lessoncancel.remote.dto.request.LessonCancelRequest
+import com.ssing.data.lesson.common.remote.dto.request.LessonCancelRequest
+import com.ssing.data.lesson.common.remote.dto.response.LessonCancellationResponse
+import com.ssing.data.lesson.common.remote.dto.response.LessonCompletionResponse
+import com.ssing.data.lesson.common.remote.dto.response.LessonStartConfirmationResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
 import retrofit2.http.Path
@@ -11,16 +13,16 @@ internal interface LessonService {
     @POST("api/v1/lessons/{lessonId}/start-confirmation")
     suspend fun lessonStart(
         @Path("lessonId") lessonId: Long,
-    ): BaseResponse<LessonResponse>
+    ): BaseResponse<LessonStartConfirmationResponse>
 
     @POST("api/v1/lessons/{lessonId}/completion")
     suspend fun lessonCompleted(
         @Path("lessonId") lessonId: Long,
-    ): BaseResponse<LessonResponse>
+    ): BaseResponse<LessonCompletionResponse>
 
     @POST("api/v1/lessons/{lessonId}/cancellation")
     suspend fun lessonCanceled(
         @Path("lessonId") lessonId: Long,
         @Body request: LessonCancelRequest
-    ): BaseResponse<LessonResponse>
+    ): BaseResponse<LessonCancellationResponse>
 }
