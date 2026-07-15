c43378665a759b8c1386323e3d3515ca8c4e49ae
doorimng <doorimng@gmail.com>
2026-07-15 17:34:14 +0900

    [feat/#139] Service request  추가

--- data/src/main/java/com/ssing/data/payment/remote/service/PaymentService.kt
+++ data/src/main/java/com/ssing/data/payment/remote/service/PaymentService.kt
@@ -5,10 +5,12 @@ import com.ssing.data.payment.remote.dto.request.PaymentRequest
 import com.ssing.data.payment.remote.dto.response.PaymentResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 internal interface PaymentService {
     @POST("/api/v1/consumer/matching-requests/{matchingRequestId}/payment")
     suspend fun postPaymentService(
+        @Path("matchingRequestId") matchingRequestId: Long,
         @Body request: PaymentRequest,
     ): BaseResponse<PaymentResponse>
 }
