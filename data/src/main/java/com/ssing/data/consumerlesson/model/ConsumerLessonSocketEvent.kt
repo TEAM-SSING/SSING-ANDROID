9962d885e6d8175da6aa875abf5fe540b8088b8c
yeseul <joyri0202@gmail.com>
2026-07-16 05:34:02 +0900

    [del/#189] 기존 파일 삭제

--- data/src/main/java/com/ssing/data/consumerlesson/model/ConsumerLessonSocketEvent.kt
+++
@@ -1,6 +0,0 @@
-package com.ssing.data.consumerlesson.model
-
-data class ConsumerLessonSocketEvent(
-    val lessonId: Long,
-    val lessonStatus: String,
-)
--- data/src/main/java/com/ssing/data/lessoncancel/repository/api/LessonCancelRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.ssing.data.lessoncancel.repository.api
-
-interface LessonCancelRepository {
-    suspend fun postLessonCancel(
-        lessonId: Long,
-        cancelReason: String,
-        cancelReasonDetail: String? = null,
-    ): Result<Unit>
-}
