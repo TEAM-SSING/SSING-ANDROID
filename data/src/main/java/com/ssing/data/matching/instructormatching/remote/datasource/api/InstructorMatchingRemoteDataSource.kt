57cc780724bc7764e02e2ebd026a2572885cd338
oilbeaneda <20221444@sungshin.ac.kr>
2026-07-15 14:51:52 +0900

    [feat/#178]  DataSource에 추가

--- data/src/main/java/com/ssing/data/matching/instructormatching/remote/datasource/api/InstructorMatchingRemoteDataSource.kt
+++ data/src/main/java/com/ssing/data/matching/instructormatching/remote/datasource/api/InstructorMatchingRemoteDataSource.kt
@@ -4,12 +4,15 @@ import com.ssing.core.network.model.BaseResponse
 import com.ssing.data.matching.instructormatching.remote.dto.request.InstructorMatchingExposureStartRequest
 import com.ssing.data.matching.instructormatching.remote.dto.response.InstructorMatchingExposureResponse
 import com.ssing.data.matching.instructormatching.remote.dto.response.InstructorMatchingExposureStartResponse
+import com.ssing.data.matching.instructormatching.remote.dto.response.InstructorMatchingOfferDetailResponse
 import com.ssing.data.matching.instructormatching.remote.dto.response.InstructorMatchingOffersResponse
 
 internal interface InstructorMatchingRemoteDataSource {
     suspend fun getMatchingExposure(): BaseResponse<InstructorMatchingExposureResponse>
 
     suspend fun getMatchingOffers(): BaseResponse<InstructorMatchingOffersResponse>
+
+    suspend fun getMatchingOfferDetail(offerId: Long): BaseResponse<InstructorMatchingOfferDetailResponse>
 
     suspend fun putMatchingExposure(
         request: InstructorMatchingExposureStartRequest,
--- data/src/main/java/com/ssing/data/matching/instructormatching/remote/datasource/impl/InstructorMatchingRemoteDataSourceImpl.kt
+++ data/src/main/java/com/ssing/data/matching/instructormatching/remote/datasource/impl/InstructorMatchingRemoteDataSourceImpl.kt
@@ -5,6 +5,7 @@ import com.ssing.data.matching.instructormatching.remote.datasource.api.Instruct
 import com.ssing.data.matching.instructormatching.remote.dto.request.InstructorMatchingExposureStartRequest
 import com.ssing.data.matching.instructormatching.remote.dto.response.InstructorMatchingExposureResponse
 import com.ssing.data.matching.instructormatching.remote.dto.response.InstructorMatchingExposureStartResponse
+import com.ssing.data.matching.instructormatching.remote.dto.response.InstructorMatchingOfferDetailResponse
 import com.ssing.data.matching.instructormatching.remote.dto.response.InstructorMatchingOffersResponse
 import com.ssing.data.matching.instructormatching.remote.service.InstructorMatchingService
 import javax.inject.Inject
@@ -23,4 +24,9 @@ internal class InstructorMatchingRemoteDataSourceImpl @Inject constructor(
 
     override suspend fun getMatchingOffers(): BaseResponse<InstructorMatchingOffersResponse> =
         instructorMatchingService.getMatchingOffers()
+
+    override suspend fun getMatchingOfferDetail(
+        offerId: Long,
+    ): BaseResponse<InstructorMatchingOfferDetailResponse> =
+        instructorMatchingService.getMatchingOfferDetail(offerId)
 }
