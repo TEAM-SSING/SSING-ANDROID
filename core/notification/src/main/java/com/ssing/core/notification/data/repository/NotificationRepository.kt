5a4a69dd03cbb75767e78870f63dc2b004e6e68a
oilbeaneda <20221444@sungshin.ac.kr>
2026-07-16 00:28:29 +0900

    [feat/#105] NotificationRepository 등록/해제 인터페이스 정리 및 패키지 이동

---
+++ core/notification/src/main/java/com/ssing/core/notification/data/repository/NotificationRepository.kt
@@ -0,0 +1,10 @@
+package com.ssing.core.notification.data.repository
+
+
+interface NotificationRepository {
+    /** 현재 기기의 FCM 토큰을 서버에 등록/갱신한다. 로그인 상태가 아니면 전송하지 않는다. */
+    suspend fun registerFcmToken(token: String): Result<Unit>
+
+    /** 현재 기기의 FCM 토큰을 서버에서 해제한다(로그아웃). */
+    suspend fun unregisterFcmToken(token: String): Result<Unit>
+}
--- core/notification/src/main/java/com/ssing/data/repository/NotificationRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.ssing.data.repository
-
-
-interface NotificationRepository {
-    suspend fun saveNotificationToken(token: String): Result<Unit>
-}
