b4a74802e0493337c1df1d0a32b72842e7d6f0e0
oilbeaneda <20221444@sungshin.ac.kr>
2026-07-16 13:13:45 +0900

    [fix/#204] 다이얼로그 방어로직 -한명이 취소하면 한명에게는 다이얼로그 안뜸

--- presentation/instructor-lesson/src/main/java/com/ssing/presentation/instructorlessondetail/LessonDetailViewModel.kt
+++ presentation/instructor-lesson/src/main/java/com/ssing/presentation/instructorlessondetail/LessonDetailViewModel.kt
@@ -82,7 +82,14 @@ internal class LessonDetailViewModel @Inject constructor(
         viewModelScope.launch {
             instructorLessonDetailRepository.fetchInstructorLessonDetail(lessonId)
                 .onSuccess { result ->
-                    updateState { copy(phase = result.toPhase()) }
+                    val phase = result.toPhase()
+                    updateState {
+                        copy(
+                            phase = phase,
+                            showLessonEndDialog = showLessonEndDialog &&
+                                phase is LessonDetailContract.LessonDetailPhase.LessonDetailOngoing,
+                        )
+                    }
                 }
                 .onFailure {
                     if (it is ApiException) {
@@ -114,6 +121,7 @@ internal class LessonDetailViewModel @Inject constructor(
     }
 
     fun onEndClick() {
+        if (uiState.value.phase !is LessonDetailContract.LessonDetailPhase.LessonDetailOngoing) return
         updateState { copy(showLessonEndDialog = true) }
     }
 
